bf1b7c4dccb9b4bc568597d0585b77ace37f06f6
Tsend16 <d.is11d102@gmail.com>
2021-08-13 12:27:18 +0800

    comment

--- middlewares/tokenShalgakh.js
+++ middlewares/tokenShalgakh.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const asyncHandler = require('express-async-handler')
 
 exports.tokenShalgakh = asyncHandler(async (req, res, next) => {
-    if (!req.headers.authorization) {
+    /*if (!req.headers.authorization) {
         throw new Error('Энэ үйлдлийг хийх эрх байхгүй байна!', 401);
     }
     const token = req.headers.authorization.split(' ')[1];
@@ -12,6 +12,6 @@ exports.tokenShalgakh = asyncHandler(async (req, res, next) => {
     if (!tokenObject.turul) {
         req.body.baiguullagiinId = tokenObject.baiguullagiinId;
         req.body.nevtersenAjiltniiToken = tokenObject;
-    }
+    }*/
     next();
 });
