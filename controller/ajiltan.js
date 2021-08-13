6687f30a02431a3d0edbb69f1ee01f09d7c75d5f
Tsend16 <d.is11d102@gmail.com>
2021-08-13 12:41:04 +0800

    jwt

--- controller/ajiltan.js
+++ controller/ajiltan.js
@@ -2,6 +2,7 @@ const asyncHandler = require('express-async-handler')
 const Ajiltan = require("../models/ajiltan");
 const License = require("../models/license");
 const aldaa = require("../components/aldaa");
+const jwt = require('jsonwebtoken');
 
 exports.ajiltanNevtrey = asyncHandler(async (req, res, next) => {
     const ajiltan = await Ajiltan.findOne()
@@ -18,8 +19,8 @@ exports.ajiltanNevtrey = asyncHandler(async (req, res, next) => {
     if (!ok)
         throw new aldaa('Хэрэглэгчийн нэр эсвэл нууц үг буруу байна!');
     let license = await License.findOne().where("baiguullagiinId").equals(ajiltan.baiguullagiinId);
-    if (!license || license.duusakhOgnoo < new Date)
-        throw new aldaa("Лицензийн хугацаа дууссан байна!")
+    // if (!license || license.duusakhOgnoo < new Date)
+    //     throw new aldaa("Лицензийн хугацаа дууссан байна!")
     const jwt = ajiltan.tokenUusgeye(license.duusakhOgnoo);
     res.status(200).json({
         duusakhOgnoo: license.duusakhOgnoo,
