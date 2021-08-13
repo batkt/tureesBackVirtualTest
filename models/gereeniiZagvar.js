d73420d9bb8af1eaefb6f1ff9e62b24c50138b3f
baterdenets <bbbatuk99@gmail.com>
2021-08-13 19:26:26 +0800

    zasvar

--- models/gereeniiZagvar.js
+++ models/gereeniiZagvar.js
@@ -1,19 +1,22 @@
 const mongoose = require("mongoose");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const Schema = mongoose.Schema;
 
 mongoose.pluralize(null);
 
-const gereeniiZaaltSchema = new Schema({
-  id: String,
-  ner: String,
-  tolgoi: String,
-  baruunTolgoi: String,
-  zuunTolgoi: String,
-  khul: String,
-  dedKhesguud: Array
-}, {
-  timestamps: true
-});
+const gereeniiZaaltSchema = new Schema(
+  {
+    id: String,
+    ner: String,
+    tolgoi: String,
+    baruunTolgoi: String,
+    zuunTolgoi: String,
+    khul: String,
+    dedKhesguud: Array,
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model("gereeniiZaalt", gereeniiZaaltSchema);
+module.exports = mongoose.model("gereeniiZagvar", gereeniiZaaltSchema);
