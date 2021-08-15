f37a56bc16e37ac12f04160971a7467b28407742
Tsend16 <d.is11d102@gmail.com>
2021-08-15 17:53:26 +0800

    nevtrekh ner

--- middlewares/aldaaBarigch.js
+++ middlewares/aldaaBarigch.js
@@ -1,9 +1,11 @@
 const aldaaBarigch = (err, req, res, next) => {
-    console.log(err);
-    res.status(err.kod || 500).json({
-        success: false,
-        aldaa: err.message
-    })
+  if (err.message.includes("indexTalbar_1 dup key"))
+    err.message = "Нэвтрэх нэр давхардаж байна!";
+  console.log(err);
+  res.status(err.kod || 500).json({
+    success: false,
+    aldaa: err.message,
+  });
 };
 
 module.exports = aldaaBarigch;
--- models/ajiltan.js
+++ models/ajiltan.js
@@ -1,55 +1,66 @@
 const mongoose = require("mongoose");
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 const Schema = mongoose.Schema;
 
 mongoose.pluralize(null);
-const ajiltanSchema = new Schema({
-  id: String,
-  ner: String,
-  ovog: String,
-  utas: String,
-  mail: String,
-  nuutsUg: {
-    type: String,
-    select: false
+const ajiltanSchema = new Schema(
+  {
+    id: String,
+    ner: String,
+    ovog: String,
+    utas: String,
+    mail: String,
+    nuutsUg: {
+      type: String,
+      select: false,
+    },
+    register: String,
+    khayag: String,
+    ajildOrsonOgnoo: Date,
+    baiguullagiinId: String,
+    baiguullagiinNer: String,
+    erkh: String,
+    firebaseToken: String,
+    albanTushaal: String,
+    zurgiinNer: String,
+    indexTalbar: {
+      type: String,
+      unique: true,
+    },
   },
-  register: String,
-  khayag: String,
-  ajildOrsonOgnoo: Date,
-  baiguullagiinId: String,
-  baiguullagiinNer: String,
-  erkh: String,
-  firebaseToken: String,
-  albanTushaal: String,
-  zurgiinNer: String
-}, {
-  timestamps: true
-});
+  {
+    timestamps: true,
+  }
+);
 
 ajiltanSchema.index({
-  '$**': 'text',
-  nuutsUg: -1
+  $nevtrekhNer: "text",
+  mail: 1,
 });
 ajiltanSchema.methods.tokenUusgeye = function (duusakhOgnoo) {
-
-  const token = jwt.sign({
-    id: this._id,
-    ner: this.ner,
-    baiguullagiinId: this.baiguullagiinId,
-    duusakhOgnoo: duusakhOgnoo
-  }, "tokenUusgexTest0123", {
-    expiresIn: '12h'
-  });
+  const token = jwt.sign(
+    {
+      id: this._id,
+      ner: this.ner,
+      baiguullagiinId: this.baiguullagiinId,
+      duusakhOgnoo: duusakhOgnoo,
+    },
+    "tokenUusgexTest0123",
+    {
+      expiresIn: "12h",
+    }
+  );
   return token;
 };
 ajiltanSchema.pre("save", async function () {
+  this.indexTalbar = this.baiguullagiinId + this.mail;
   const salt = await bcrypt.genSalt(12);
   this.nuutsUg = await bcrypt.hash(this.nuutsUg, salt);
-})
+});
 
 ajiltanSchema.methods.passwordShalgaya = async function (pass) {
-  return await bcrypt.compare(pass, this.nuutsUg)
-}
+  return await bcrypt.compare(pass, this.nuutsUg);
+};
 
 module.exports = mongoose.model("ajiltan", ajiltanSchema);
