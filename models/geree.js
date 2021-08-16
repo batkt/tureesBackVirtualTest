1e4f3153e4a087589cc696dec71cd4a796bd2ae9
baterdenets <bbbatuk99@gmail.com>
2021-08-16 14:13:29 +0800

    zasvar

--- models/geree.js
+++ models/geree.js
@@ -1,30 +1,38 @@
 const mongoose = require("mongoose");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const Schema = mongoose.Schema;
 
 mongoose.pluralize(null);
-const gereeSchema = new Schema({
-  id: String,
-  gereeniiDugaar: String,
-  gereeniiOgnoo: String,
-  turul: String,
-  ovog: String,
-  ner: String,
-  register: String,
-  albanTushaal: String,
-  zakhirliinOvog: String,
-  zakhirliinNer: String,
-  utas: String,
-  khayag: String,
-  khugatsaa: String,
-  duusakhOgnoo: String,
-  khungulukhKhugatsaa: String,
-  sariinTurees: String,
-  gerchilgeeniiZurag: String,
-  unemlekhniiZurag: String,
-  zuvshuurliinZurag: String
-}, {
-  timestamps: true
-});
+const gereeSchema = new Schema(
+  {
+    id: String,
+    gereeniiDugaar: String,
+    gereeniiOgnoo: String,
+    turul: String,
+    ovog: String,
+    ner: String,
+    register: String,
+    albanTushaal: String,
+    zakhirliinOvog: String,
+    zakhirliinNer: String,
+    utas: String,
+    khayag: String,
+    khugatsaa: String,
+    duusakhOgnoo: String,
+    khungulukhKhugatsaa: String,
+    sariinTurees: String,
+    gerchilgeeniiZurag: String,
+    unemlekhniiZurag: String,
+    zuvshuurliinZurag: String,
+    languuniiDugaar: String,
+    talbainNegjUne: String,
+    talbainNiitUne: String,
+    talbainKhemjee: String,
+    davkhar: String,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("geree", gereeSchema);
