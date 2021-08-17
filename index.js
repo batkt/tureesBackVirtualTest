63fe594e82ca14292b0661739b83953fb2d414aa
Tsend16 <d.is11d102@gmail.com>
2021-08-17 20:15:56 +0800

    khariltsagchRoute

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const http = require('http');
+const http = require("http");
 const cors = require("cors");
 const server = http.Server(app);
-const io = require('socket.io')(server);
+const io = require("socket.io")(server);
 
 const baiguullagaRoute = require("./routes/baiguullagaRoute");
 const ajiltanRoute = require("./routes/ajiltanRoute");
@@ -14,27 +14,29 @@ const tailanRoute = require("./routes/tailanRoute");
 const gereeRoute = require("./routes/gereeRoute");
 const gereeniiZagvarRoute = require("./routes/gereeniiZagvarRoute");
 const languuRoute = require("./routes/languuRoute");
+const khariltsagchRoute = require("./routes/khariltsagchRoute");
 
 const dbUrl =
-    "mongodb://localhost:27017/turees?readPreference=primary&ssl=false";
+  "mongodb://localhost:27017/turees?readPreference=primary&ssl=false";
 mongoose
-    .connect(dbUrl, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-    })
-    .then((result) => server.listen(8081))
-    .catch((err) => console.log(err));
+  .connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((result) => server.listen(8081))
+  .catch((err) => console.log(err));
 
 process.env.TZ = "Asia/Ulaanbaatar";
 
-
-app.set('socketio', io);
+app.set("socketio", io);
 app.use(cors());
-app.use(express.json({
-    limit: '50mb'
-}));
+app.use(
+  express.json({
+    limit: "50mb",
+  })
+);
 app.use(baiguullagaRoute);
 app.use(ajiltanRoute);
 app.use(licenseRoute);
@@ -43,10 +45,11 @@ app.use(aldaaBarigch);
 app.use(gereeRoute);
 app.use(gereeniiZagvarRoute);
 app.use(languuRoute);
+app.use(khariltsagchRoute);
 
-io.on('connection', (socket) => {
-    console.log('connected');
-    socket.on('disconnect', () => {
-        console.log('disconnected');
-    });
+io.on("connection", (socket) => {
+  console.log("connected");
+  socket.on("disconnect", () => {
+    console.log("disconnected");
+  });
 });
