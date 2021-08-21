95ec1b6286decba35453c54ac261fce9e5e46611
Tsend16 <d.is11d102@gmail.com>
2021-08-21 14:56:50 +0800

    khurunguud

--- models/languu.js
+++ models/languu.js
@@ -1,9 +1,10 @@
-const mongoose = require("mongoose")
-const jwt = require("jsonwebtoken")
-const bcrypt = require("bcrypt")
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const Schema = mongoose.Schema;
 
-mongoose.pluralize(null)
+mongoose.pluralize(null);
+
 const languuSchema = new Schema(
   {
     id: String,
@@ -15,10 +16,19 @@ const languuSchema = new Schema(
     baiguullagiinNer: String,
     talbainNegjUne: Number,
     talbainNiitUne: Number,
+    khurunguud: [
+      {
+        id: String,
+        ner: String,
+        too: Number,
+        une: Number,
+        niit: Number,
+      },
+    ],
   },
   {
     timestamps: true,
   }
-)
+);
 
-module.exports = mongoose.model("languu", languuSchema)
+module.exports = mongoose.model("languu", languuSchema);
