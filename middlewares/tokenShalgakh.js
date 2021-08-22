ba23e87c8780d17b12e18f648e428bc7681de054
Tsend16 <d.is11d102@gmail.com>
2021-08-22 12:41:13 +0800

    token shalgax comment

--- middlewares/tokenShalgakh.js
+++ middlewares/tokenShalgakh.js
@@ -1,17 +1,16 @@
-const jwt = require('jsonwebtoken');
-const asyncHandler = require('express-async-handler')
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
 
 exports.tokenShalgakh = asyncHandler(async (req, res, next) => {
-    /*if (!req.headers.authorization) {
-        throw new Error('Энэ үйлдлийг хийх эрх байхгүй байна!', 401);
-    }
-    const token = req.headers.authorization.split(' ')[1];
-    if (!token)
-        throw new Error('Токен байхгүй байна!');
-    const tokenObject = jwt.verify(token, 'tokenUusgexTest0123', 401);
-    if (!tokenObject.turul) {
-        req.body.baiguullagiinId = tokenObject.baiguullagiinId;
-        req.body.nevtersenAjiltniiToken = tokenObject;
-    }*/
-    next();
+  if (!req.headers.authorization) {
+    throw new Error("Энэ үйлдлийг хийх эрх байхгүй байна!", 401);
+  }
+  const token = req.headers.authorization.split(" ")[1];
+  if (!token) throw new Error("Токен байхгүй байна!");
+  const tokenObject = jwt.verify(token, "tokenUusgexTest0123", 401);
+  if (!tokenObject.turul) {
+    req.body.baiguullagiinId = tokenObject.baiguullagiinId;
+    req.body.nevtersenAjiltniiToken = tokenObject;
+  }
+  next();
 });
