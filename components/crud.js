6cedb55afe74a7a57979330cc22faf3fa41a75b4
Tsend16 <d.is11d102@gmail.com>
2021-09-03 21:26:20 +0800

    aldaa barigch

--- components/crud.js
+++ components/crud.js
@@ -94,11 +94,11 @@ function crud(
   router.get(`/${modelName}`, tokenShalgakh, async (req, res, next) => {
     try {
       const body = req.query;
-      if(!!body?.query) body.query = JSON.parse(body.query);
-      if(!!body?.order) body.order = JSON.parse(body.order);
-      if(!!body?.khuudasniiDugaar) body.khuudasniiDugaar = Number(body.khuudasniiDugaar);
-      if(!!body?.khuudasniiKhemjee) body.khuudasniiKhemjee = Number(body.khuudasniiKhemjee);
-      if(!!body?.search) body.search = String(body.search);
+      if (!!body?.query) body.query = JSON.parse(body.query);
+      if (!!body?.order) body.order = JSON.parse(body.order);
+      if (!!body?.khuudasniiDugaar) body.khuudasniiDugaar = Number(body.khuudasniiDugaar);
+      if (!!body?.khuudasniiKhemjee) body.khuudasniiKhemjee = Number(body.khuudasniiKhemjee);
+      if (!!body?.search) body.search = String(body.search);
       khuudaslalt(Model, body)
         .then((result) => {
           res.send(result);
@@ -153,8 +153,8 @@ function crudWithFile(
 ) {
   router.post(
     `/${modelName}`,
-    tokenShalgakh,
     upload.single(fileProp.fileName),
+    tokenShalgakh,
     async (req, res, next) => {
       try {
         const data = new Model(req.body);
@@ -181,8 +181,8 @@ function crudWithFile(
   );
   router.put(
     `/${modelName}/:id`,
-    tokenShalgakh,
     upload.single(fileProp.fileName),
+    tokenShalgakh,
     async (req, res, next) => {
       try {
         const data = new Model(req.body);
@@ -214,11 +214,11 @@ function crudWithFile(
   router.get(`/${modelName}`, tokenShalgakh, async (req, res, next) => {
     try {
       const body = req.query;
-      if(!!body?.query) body.query = JSON.parse(body.query);
-      if(!!body?.order) body.order = JSON.parse(body.order);
-      if(!!body?.khuudasniiDugaar) body.khuudasniiDugaar = Number(body.khuudasniiDugaar);
-      if(!!body?.khuudasniiKhemjee) body.khuudasniiKhemjee = Number(body.khuudasniiKhemjee);
-      if(!!body?.search) body.search = String(body.search);
+      if (!!body?.query) body.query = JSON.parse(body.query);
+      if (!!body?.order) body.order = JSON.parse(body.order);
+      if (!!body?.khuudasniiDugaar) body.khuudasniiDugaar = Number(body.khuudasniiDugaar);
+      if (!!body?.khuudasniiKhemjee) body.khuudasniiKhemjee = Number(body.khuudasniiKhemjee);
+      if (!!body?.search) body.search = String(body.search);
       khuudaslalt(Model, body)
         .then((result) => {
           res.send(result);
--- middlewares/aldaaBarigch.js
+++ middlewares/aldaaBarigch.js
@@ -1,7 +1,45 @@
+const http = require("http");
+
+function aldaagIlgeeye(aldaa, req) {
+  const data = new TextEncoder().encode(JSON.stringify({
+    system: "Turees",
+    aldaa: aldaa,
+    aldaaniiMsg: aldaa.message,
+    ognoo: new Date(),
+    baiguullagiinId: req.body.baiguullagiinId,
+    burtgesenAjiltaniiId: req.body.nevtersenAjiltniiToken.id,
+    burtgesenAjiltaniiNer: req.body.nevtersenAjiltniiToken.ner,
+  }));
+  const options = {
+    hostname: "127.0.0.1",
+    port: 8282,
+    path: "/aldaa",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  const request = http.request(options, (response) => {
+    response.on("data", (d) => {
+      if (response.statusCode == 200) {
+        console.log(d);
+        console.log("aldaag shidlee!");
+      }
+    });
+  });
+  request.on("error", (error) => {
+    throw new aldaa(error);
+  });
+
+  request.write(data);
+  request.end();
+};
 const aldaaBarigch = (err, req, res, next) => {
+  console.log("end irsen", req.body)
+  if (req.body.nevtersenAjiltniiToken)
+    aldaagIlgeeye(err, req);
   if (err.message.includes("indexTalbar_1 dup key"))
     err.message = "Нэвтрэх нэр давхардаж байна!";
-  console.log(err);
   res.status(err.kod || 500).json({
     success: false,
     aldaa: err.message,
