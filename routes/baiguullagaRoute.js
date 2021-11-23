eb403387d4aa6254b743a39afc3cc83b6ee5fd52
baterdenets <bbbatuk99@gmail.com>
2021-11-23 15:00:56 +0800

    zasvar

--- routes/baiguullagaRoute.js
+++ routes/baiguullagaRoute.js
@@ -55,7 +55,7 @@ router.post(
       if(!!req.body)
         {
           const {baiguullagiinId,tokhirgoo} = req.body
-          Baiguullaga.findOneAndUpdate({_id:baiguullagiinId},{$set:tokhirgoo}).then()
+          Baiguullaga.findOneAndUpdate({_id:baiguullagiinId},{$set:tokhirgoo}).then(()=>res.send("Amjilttai"))
         }
       else
         next(new aldaa("Засах боломжгүй байна"))
