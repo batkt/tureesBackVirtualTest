ecf659bea79352f268e46e1a89f5bebdf8a561f3
Tsend16 <d.is11d102@gmail.com>
2022-01-17 14:40:19 +0800

    a

--- kholbolt/tdbcer.js
+++ kholbolt/tdbcer.js
@@ -40,8 +40,7 @@ exports.tdbcer = asyncHandler(async (req, res, next) => {
             path: '/api/trusted',
             method: 'POST',
             key: fs.readFileSync("./kholbolt/corptdb.key"),
-            cert: fs.readFileSync("./kholbolt/corptdb.cer"),
-            pass: "1234",
+            cert: fs.readFileSync("./kholbolt/corptdb.pem"),
             headers: {
                 'Content-Type': 'text/xml; charset=utf-8',
                 'Content-Length': Buffer.byteLength(xml)
