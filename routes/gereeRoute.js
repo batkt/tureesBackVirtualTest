0daa89b5857cbd2118ad4a6fbb900d15b62f6786
Tsend16 <d.is11d102@gmail.com>
2022-01-20 13:12:07 +0800

    baritsaa

--- routes/gereeRoute.js
+++ routes/gereeRoute.js
@@ -69,6 +69,17 @@ router.route("/gereeniiTulultAvya/:gereeniiId").get(tokenShalgakh, (req, res, ne
     next(err);
   });
 });
+router.route("/baritsaaTulultAvya/:gereeniiId").get(tokenShalgakh, (req, res, next) => {
+  Geree.findById(req.params.gereeniiId).select('avlaga').then((result) => {
+    if (lodash.isArray(lodash.get(result, 'avlaga.baritsaa'))) {
+      var a = lodash.get(result, 'avlaga.baritsaa').filter(a => a.ognoo < new Date(req.query.duusakhOgnoo));
+      a = lodash.orderBy(a, ['ognoo'], ['asc']);
+      res.send(a)
+    }
+  }).catch((err) => {
+    next(err);
+  });
+});
 
 router.route("/nekhemjlekhiinDugaarlaltAvya").get(tokenShalgakh, async (req, res, next) => {
   try {
