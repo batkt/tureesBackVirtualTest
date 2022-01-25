c5deea3c6800aad030583a67f3a0ff026f353fc7
Tsend16 <d.is11d102@gmail.com>
2022-01-25 10:58:42 +0800

    dans

--- models/dans.js
+++ models/dans.js
@@ -3,24 +3,24 @@ const Schema = mongoose.Schema;
 
 mongoose.pluralize(null);
 const dansSchema = new Schema({
-  dugaar: Number,
-  dansniiNer:String,
+  dugaar: String,
+  dansniiNer: String,
   baiguullagiinId: String,
   barilgiinId: String,
   bank: String,
-  valiut:String,
-  corporateAshiglakhEsekh:Boolean,
-  corporateNevtrekhNer:{
-    select:false,
-    type:String
+  valyut: String,
+  corporateAshiglakhEsekh: Boolean,
+  corporateNevtrekhNer: {
+    select: false,
+    type: String
   },
-  corporateNuutsUg:{
-    select:false,
-    type:String
+  corporateNuutsUg: {
+    select: false,
+    type: String
   },
-  corporateGuilgeeniiNuutsUg:{
-    select:false,
-    type:String
+  corporateGuilgeeniiNuutsUg: {
+    select: false,
+    type: String
   },
 }, {
   timestamps: true
