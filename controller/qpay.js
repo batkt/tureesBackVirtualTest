6d687f61ddb23dc57ef737bc214f08434fe8d3ce
Tsend16 <d.is11d102@gmail.com>
2022-01-25 20:58:28 +0800

    log

--- controller/qpay.js
+++ controller/qpay.js
@@ -120,6 +120,7 @@ async function qpayObjectUusgeye(body, invoiceCode, next) {
 }
 
 exports.qpayGargaya = asyncHandler(async (req, res, next) => {
+    console.log("qpayGargaya", req.body);
     var dans = await Dans.findOne({ dugaar: req.body.dansniiDugaar });
     if (!dans.qpayAshiglakhEsekh || !dans.qpayUsername || !dans.qpayPassword || !dans.qpayInvoiceCode)
         throw new aldaa("Qpay тохиргоо хийгдээгүй байна!");
