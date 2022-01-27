ad85ef1ffcc249b4f9998cb7698e0b6160cae3e9
Tsend16 <d.is11d102@gmail.com>
2022-01-27 15:02:05 +0800

    5min

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ app.use(dansRoute);
 app.use(aldaaBarigch);
 
 cron.schedule(
-  "*/2 * * * * ",
+  "*/5 * * * * ",
   function () {
     console.log("xuulga tatlaa", new Date());
     /*cgw.bankniiKhuulgaTatajKhadgalya(
