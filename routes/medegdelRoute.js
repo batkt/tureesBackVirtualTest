5c90d3423960b5379a82a90851c421eef92d95c0
Tsend16 <d.is11d102@gmail.com>
2022-01-28 10:54:13 +0800

    crud sonorduulga

--- routes/medegdelRoute.js
+++ routes/medegdelRoute.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const SanalGomdol = require("../models/sanalGomdol");
+const Sonorduulga = require("../components/sonorduulga");
 const { tokenShalgakh } = require("../middlewares/tokenShalgakh");
 const { crud } = require("../components/crud");
 
 const { sanalKhadgalya, sanalKharlaa } = require("../controller/medegdel");
 
 crud(router, "sanalGomdol", SanalGomdol);
+crud(router, "sonorduulga", Sonorduulga);
 
 router.route("/sanalKhadgalya").post(tokenShalgakh, sanalKhadgalya);
 router.route("/sanalKharlaa").post(tokenShalgakh, sanalKharlaa);
