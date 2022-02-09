87195032ffbd5755049fa4555c817b85f506da25
Tsend16 <d.is11d102@gmail.com>
2022-02-09 18:16:29 +0800

    avlagiinTailanAvya

--- controller/tailan.js
+++ controller/tailan.js
@@ -89,3 +89,99 @@ exports.guitsetgeliinTailanAvya = asyncHandler(async (req, res, next) => {
         next(err);
     });;
 });
+
+exports.avlagiinTailanAvya = asyncHandler(async (req, res, next) => {
+    let query = [
+        {
+            '$match': {
+                'baiguullagiinId': req.body.baiguullagiinId,
+                'barilgiinId': req.body.barilgiinId
+            }
+        }, {
+            '$unwind': {
+                'path': '$avlaga.guilgeenuud'
+            }
+        }, {
+            '$unwind': {
+                'path': '$avlaga.guilgeenuud.ognoo'
+            }
+        },
+        {
+            '$match': {
+                "avlaga.guilgeenuud.ognoo": {
+                    $lte: new Date(req.body.duusakhOgnoo)
+                },
+                "avlaga.guilgeenuud.turul": {
+                    $nin: ["baritsaa"]
+                }
+            }
+        }, {
+            '$group': {
+                '_id': {
+                    'year': {
+                        $year: { date: "$avlaga.guilgeenuud.ognoo", timezone: "Asia/Ulaanbaatar" }
+                    },
+                    'month': {
+                        $month: { date: "$avlaga.guilgeenuud.ognoo", timezone: "Asia/Ulaanbaatar" }
+                    }
+                }, 'tulukh': {
+                    '$sum': '$avlaga.guilgeenuud.tulukhDun'
+                },
+                'tulsun': {
+                    '$sum': '$avlaga.guilgeenuud.tulsunDun'
+                }
+            }
+        }, {
+            '$sort': {
+                '_id.year': 1,
+                '_id.month': 1
+            }
+        }
+    ]
+    Geree.aggregate(query).then((result) => {
+        if (result && result.length > 0) {
+            var labels = []
+            var tuluvluguunuud = []
+            var guitsetgeluud = []
+            console.log("result", result);
+            var ekhlekhSar = new Date(req.body.ekhlekhOgnoo).getMonth() + 1; // returns 0 - 11
+            var ekhlekhOn = new Date(req.body.ekhlekhOgnoo).getFullYear();
+            var niitAvlaga = 0;
+            var niitTulsun = 0;
+            result.forEach((a) => {
+                niitAvlaga = niitAvlaga + a.tulukh;
+                niitTulsun = niitTulsun + a.tulsun;
+                if (a["_id"].year >= ekhlekhOn && a["_id"].month >= ekhlekhSar) {
+                    labels.push(a["_id"].year + "-" + a["_id"].month);
+                    tuluvluguunuud.push(niitAvlaga.toFixed(2));
+                    guitsetgeluud.push(niitTulsun.toFixed(2));
+                }
+            });
+            var data = {
+                labels,
+                datasets: [
+                    {
+                        label: "Төлөвлөгөө",
+                        data: tuluvluguunuud,
+                        backgroundColor: "rgba(255, 99, 132, 0.5)",
+                        borderColor: "rgba(255, 99, 132, 0.5)",
+                        fill: false,
+                        lineWidth: 10,
+                    },
+                    {
+                        label: "Гүйцэтгэл",
+                        data: guitsetgeluud,
+                        fill: false,
+                        borderColor: "rgba(53, 162, 235, 0.5)",
+                        backgroundColor: "rgba(53, 162, 235, 0.5)",
+                        borderDash: [10, 10]
+                    },
+                ],
+            }
+            res.send(data);
+        }
+        res.send(result);
+    }).catch((err) => {
+        next(err);
+    });;
+});
--- routes/tailanRoute.js
+++ routes/tailanRoute.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { tokenShalgakh } = require("zevback");
-const { guitsetgeliinTailanAvya } = require("../controller/tailan");
+const { guitsetgeliinTailanAvya, avlagiinTailanAvya } = require("../controller/tailan");
 
 router.route("/guitsetgeliinTailanAvya").post(tokenShalgakh, guitsetgeliinTailanAvya);
+router.route("/avlagiinTailanAvya").post(tokenShalgakh, avlagiinTailanAvya);
 module.exports = router;
