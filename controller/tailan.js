c8bc2870fd9d9ea87e42d4a8a245caea85a59a87
Tsend16 <d.is11d102@gmail.com>
2022-02-10 12:18:41 +0800

    aa

--- controller/tailan.js
+++ controller/tailan.js
@@ -28,7 +28,7 @@ exports.borluulaltiinTailanAvya = asyncHandler(async (req, res, next) => {
         sort['_id.year'] = 1
         sort['_id.month'] = 1
     }
-    else if (req.body.nariivchlal == "month") {
+    else if (req.body.nariivchlal == "day") {
         group['_id']['year'] = {
             $year: { date: "$avlaga.guilgeenuud.ognoo", timezone: "Asia/Ulaanbaatar" }
         }
@@ -42,6 +42,7 @@ exports.borluulaltiinTailanAvya = asyncHandler(async (req, res, next) => {
         sort['_id.month'] = 1
         sort['_id.day'] = 1
     }
+    console.log(sort);
     let query = [
         {
             '$match': {
