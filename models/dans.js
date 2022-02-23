06c7e4772ad74e52a5adf2ba26ca72a4715d74e5
Tsend16 <d.is11d102@gmail.com>
2022-02-23 10:20:33 +0800

    anybic nemlee

--- models/dans.js
+++ models/dans.js
@@ -10,6 +10,8 @@ const dansSchema = new Schema({
   bank: String,
   valyut: String,
   corporateAshiglakhEsekh: Boolean,
+  corporateAnyBIC: String,
+  corporateRoleID: String,
   corporateNevtrekhNer: {
     //select: false,
     type: String
