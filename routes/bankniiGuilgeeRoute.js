bef144f4ac5e060a7053be1f3fcf48a1d2aeb2cf
Tsend16 <d.is11d102@gmail.com>
2022-03-31 16:09:50 +0800

    dansniiKhuulgaDunAvya

--- routes/bankniiGuilgeeRoute.js
+++ routes/bankniiGuilgeeRoute.js
@@ -12,4 +12,74 @@ crud(router, 'bankniiGuilgee', BankniiGuilgee, UstsanBarimt)
 router.post("/bankniiGuilgeeToololtAvya", tokenShalgakh, bankniiGuilgeeToololtAvya);
 router.post("/tdbcer", tdbcer);
 
+router.route("/dansniiKhuulgaDunAvya").post(tokenShalgakh, async (req, res, next) => {
+    var turul = req.body.turul;
+    let query = [
+        {
+            '$match': {
+                'baiguullagiinId': req.body.baiguullagiinId,
+                'barilgiinId': req.body.barilgiinId,
+                "dansniiDugaar": req.body.dansniiDugaar,
+                "$or": [
+                    {
+                        "$and": [
+                            {
+                                "TxDt": {
+                                    $gte: new Date(req.body.ekhlekhOgnoo),
+                                    $lte: new Date(req.body.duusakhOgnoo)
+                                }
+                            },
+                            {
+                                "Amt": (turul == "orlogo") ? {
+                                    $gt: 0
+                                } : {
+                                    $lt: 0
+                                }
+                            }
+                        ]
+                    },
+                    {
+                        "$and": [
+                            {
+                                "tranDate": {
+                                    $gte: new Date(req.body.ekhlekhOgnoo),
+                                    $lte: new Date(req.body.duusakhOgnoo)
+                                }
+                            },
+                            {
+                                "amount": (turul == "orlogo") ? {
+                                    $gt: 0
+                                } : {
+                                    $lt: 0
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        },
+        {
+            '$project': {
+                "dun": { "$ifNull": ["$Amt", "$amount"] }
+            }
+        },
+        {
+            '$group': {
+                '_id': "dun",
+                'dun': {
+                    $sum: "$dun"
+                }
+            }
+        }
+    ]
+    console.log("turluur", JSON.stringify(query, null, 4));
+    BankniiGuilgee.aggregate(query).then((result) => {
+        res.send(result);
+    })
+        .catch((err) => {
+            next(err);
+        });;
+});
+
+
 module.exports = router;
