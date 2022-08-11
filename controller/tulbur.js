3ff5e002eae1dd976781b11afa72b9f7361cadc5
Tsend16 <d.is11d102@gmail.com>
2022-08-11 10:10:58 +0800

    daraagiin tulux ognoo

--- controller/tulbur.js
+++ controller/tulbur.js
@@ -1026,22 +1026,31 @@ exports.gereeAutomataarSungaya = asyncHandler(
   }
 );
 
+async function tooZasya(too) {
+  var zassanToo = await Math.round((too + Number.EPSILON) * 100) / 100;
+  return +zassanToo.toFixed(2)
+}
+
 async function daraagiinTulukhOgnooZasya(gereeniiId) {
   var geree = await Geree.findById(gereeniiId).select("avlaga");
   var jagsaalt = [];
   if (lodash.isArray(lodash.get(geree, "avlaga.guilgeenuud"))) {
-    jagsaalt = lodash.get(geree, "avlaga.guilgeenuud");
+    jagsaalt = await lodash.get(geree, "avlaga.guilgeenuud");
   }
-  var niitTulsunDun = lodash.sumBy(jagsaalt, function (object) {
-    return object.tulsunDun;
+  var niitTulsunDun = await lodash.sumBy(jagsaalt, function (object) {
+    return (object.tulsunDun) ? object.tulsunDun : 0;
   });
+  var niitKhyamdral = await lodash.sumBy(jagsaalt, function (object) {
+    return (object.khyamdral) ? object.khyamdral : 0;
+  });
+  niitTulsunDun = await tooZasya(niitTulsunDun + niitKhyamdral);
   jagsaalt = lodash.filter(jagsaalt, (a) => a.tulukhDun != null);
   jagsaalt = lodash.orderBy(jagsaalt, ["ognoo"], ["asc"]);
   var tulukhOgnoo;
-  jagsaalt.forEach((element) => {
+  jagsaalt.forEach(async (element) => {
     if (niitTulsunDun > 0) {
       tulukhOgnoo = element.ognoo;
-      niitTulsunDun = niitTulsunDun - element.tulukhDun;
+      niitTulsunDun = await tooZasya(niitTulsunDun - element.tulukhDun);
     }
   });
   Geree.findByIdAndUpdate(gereeniiId, {
