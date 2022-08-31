47e95e6df92c41748a9e01f3852f1df9d9625b00
Tsend16 <d.is11d102@gmail.com>
2022-08-31 16:24:09 +0800

    aa

--- routes/dansRoute.js
+++ routes/dansRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const dans = require("../models/dans");
+const { dans } = require("zevback");
 //const { crud } = require("../components/crud");
 //const UstsanBarimt = require("../models/ustsanBarimt");
 const { crud, UstsanBarimt } = require("zevback");
