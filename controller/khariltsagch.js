f0922e7afc0b4f9882229d81206908223e5f8103
Tsend16 <d.is11d102@gmail.com>
2022-09-15 17:22:29 +0800

    await

--- controller/khariltsagch.js
+++ controller/khariltsagch.js
@@ -93,7 +93,7 @@ exports.sergeekhKodAvya = asyncHandler(async (req, res, next) => {
     if (!msgIlgeekhKey || !msgIlgeekhDugaar)
       throw new aldaa("Мсж илгээх тохиргоо хийгдээгүй байна!");
     await Khariltsagch.updateOne({ _id: khariltsagch._id }, { $set: { sergeekhKod: khariltsagch.sergeekhKod } });
-    msgIlgeeye([{ text: khariltsagch.sergeekhKod, to: khariltsagch?.utas }], msgIlgeekhKey, msgIlgeekhDugaar, [], 0, next, res)
+    await msgIlgeeye([{ text: khariltsagch.sergeekhKod, to: khariltsagch?.utas }], msgIlgeekhKey, msgIlgeekhDugaar, [], 0, next, res)
     res.send(khariltsagch._id)
   } catch (err) {
     next(err);
