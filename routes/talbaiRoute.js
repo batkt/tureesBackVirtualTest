0dae1abfd3797e942b280de565529637781b782d
Tsend16 <d.is11d102@gmail.com>
2022-10-19 14:42:12 +0800

    talbai shalguur

--- routes/talbaiRoute.js
+++ routes/talbaiRoute.js
@@ -32,7 +32,8 @@ const { talbaiTatya, talbainZagvarAvya } = require("../controller/excel");
 router.route("/talbaiTatya").post(uploadFile.single("file"), tokenShalgakh, talbaiTatya);
 router.route("/talbainZagvarAvya").get(tokenShalgakh, talbainZagvarAvya);
 router.route("/talbainSulEskhiigShalgay").get(tokenShalgakh, async (req, res, next) => {
-    var geree = await Geree.findOne({ talbainDugaar: req.query.talbainDugaar, barilgiinId: req.query.barilgiinId, tuluv: 1, duusakhOgnoo: { $gte: new Date() } });
+    var talbai = await Talbai.find({ talbainDugaar: req.query.talbainDugaar, barilgiinId: req.query.barilgiinId }).lean();
+    var geree = await Geree.findOne({ talbainIdnuud: talbai._id, barilgiinId: req.query.barilgiinId, tuluv: 1, duusakhOgnoo: { $gte: new Date() } });
     if (geree)
         res.send(geree.gereeniiDugaar);
     else
