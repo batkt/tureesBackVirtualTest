ec67031a76b8c5282a947fc1625f53c3988d4dd8
Tsend16 <d.is11d102@gmail.com>
2022-10-19 10:13:15 +0800

    zasaw

--- controller/toololt.js
+++ controller/toololt.js
@@ -95,7 +95,7 @@ exports.gereeniiToololtAvya = asyncHandler(async (req, res, next) => {
   ]
   Geree.aggregate(query).then((result) => {
     if (result && result.length > 0) {
-      if (result[0].tsutsalsan && result[0].tsutsalsan.length > 0)
+      if (result[0].tsutsalsan && result[0].tsutsalsan.length > 0 && result[0].busad && result[0].busad.length > 0)
         result[0].busad[0].tsutsalsan = result[0].tsutsalsan[0].tsutsalsan;
       result = result[0].busad;
     }
