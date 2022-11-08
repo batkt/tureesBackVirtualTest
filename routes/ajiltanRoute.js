f8633b71e7d71f6a9933e6f8a157baa5a8a4bec3
Tsend16 <d.is11d102@gmail.com>
2022-11-08 12:13:19 +0800

    gz

--- routes/ajiltanRoute.js
+++ routes/ajiltanRoute.js
@@ -207,7 +207,7 @@ router.post('/backAvya', tokenShalgakh, async (req, res, next) => {
       }
     });*/
 
-    var backupDB = exec('mongodump --host=' + "localhost" + ' --port=' + "27017" + ' --db=' + "turees" + ' --archive=' + "file/tmp" + '/' + "dump.tar" + '.gz  --gzip',
+    var backupDB = exec('mongodump --host=' + "localhost" + ' --port=' + "27017" + ' --db=' + "turees" + ' --archive=' + "file/tmp" + '/' + "dump.tar" + '  --gzip',
       (err, stdout, stderr) => {
         console.log("err -->", err)
         console.log("stdout -->", stdout)
