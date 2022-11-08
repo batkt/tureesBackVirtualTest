5ef9f02059bf9c37a4133fdf0aadf3f941d6e568
Tsend16 <d.is11d102@gmail.com>
2022-11-08 15:25:56 +0800

    ?

--- controller/toololt.js
+++ controller/toololt.js
@@ -116,8 +116,8 @@ exports.gereeniiToololtAvya = asyncHandler(async (req, res, next) => {
         result[0].busad[0].tsutsalsan = result[0].tsutsalsan[0].tsutsalsan;
       result = result[0].busad;
       if (turGeree && turGeree.length > 0) {
-        result[0].turGeree = turGeree.find((a) => a._id).too;
-        result[0].undsenGeree = turGeree.find((a) => !a._id).too;
+        result[0].turGeree = turGeree.find((a) => a._id)?.too;
+        result[0].undsenGeree = turGeree.find((a) => !a._id)?.too;
       }
     }
     res.send(result);
