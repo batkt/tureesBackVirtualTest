b0fb9b41fe6527822f849c84ee42ebe02b60f5a5
Tsend16 <d.is11d102@gmail.com>
2022-12-15 14:00:01 +0800

    tuukh.ip

--- controller/ajiltan.js
+++ controller/ajiltan.js
@@ -57,8 +57,8 @@ exports.ajiltanNevtrey = asyncHandler(async (req, res, next) => {
           tuukh.ognoo = new Date();
           tuukh.uildliinSystem = ua.os;
           tuukh.ip = req.ip;
-          if (ip.substr(0, 7) == "::ffff:") {
-            ip = ip.substr(7);
+          if (tuukh.ip.substr(0, 7) == "::ffff:") {
+            tuukh.ip = tuukh.ip.substr(7);
           }
           tuukh.browser = ua.browser;
           tuukh.useragent = ua;
