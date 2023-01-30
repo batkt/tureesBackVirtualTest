6085948069335e27336d45600fcb05082d7e5248
Tsend16 <d.is11d102@gmail.com>
2023-01-30 14:03:36 +0800

    aa

--- models/qpayObject.js
+++ models/qpayObject.js
@@ -2,15 +2,19 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 mongoose.pluralize(null);
-const qpayObjectSchema = new Schema({
-  gereeniiId: String,
-  baiguullagiinId: String,
-  barilgiinId: String,
-  tulsunEsekh: Boolean,
-  ognoo: Date,
-  qpay: Schema.Types.Mixed
-}, {
-  timestamps: true
-});
+const qpayObjectSchema = new Schema(
+  {
+    gereeniiId: String,
+    baiguullagiinId: String,
+    barilgiinId: String,
+    tulsunEsekh: Boolean,
+    ognoo: Date,
+    qpay: Schema.Types.Mixed,
+    payment_id: String,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("qpayObject", qpayObjectSchema);
