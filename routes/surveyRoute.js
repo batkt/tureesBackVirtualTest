212d6a31af4be6e4185ba0354f4a5812a42ee2ab
Tsend16 <d.is11d102@gmail.com>
2023-02-08 09:14:30 +0800

    err

--- routes/surveyRoute.js
+++ routes/surveyRoute.js
@@ -11,31 +11,39 @@ crud(router, "asuult", Asuult, UstsanBarimt);
 crud(router, "khariult", Khariult, UstsanBarimt);
 
 router.route("/surveyKhadgalya").post(async (req, res, next) => {
-  const { db } = require("zevbackv2");
-  var kholboltuud = db.kholboltuud;
-  var tukhainBaaziinKholbolt = kholboltuud.find(
-    (a) => a.baiguullagiinId == req.body.baiguullagiinId
-  );
-  const data = new Khariult(tukhainBaaziinKholbolt)(req.body);
-  data
-    .save()
-    .then((result) => res.send("Amjilttai"))
-    .catch((err) => next(err));
+  try {
+    const { db } = require("zevbackv2");
+    var kholboltuud = db.kholboltuud;
+    var tukhainBaaziinKholbolt = kholboltuud.find(
+      (a) => a.baiguullagiinId == req.body.baiguullagiinId
+    );
+    const data = new Khariult(tukhainBaaziinKholbolt)(req.body);
+    data
+      .save()
+      .then((result) => res.send("Amjilttai"))
+      .catch((err) => next(err));
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.route("/asuultAvya/:baiguullagiinId/:id").get(async (req, res, next) => {
-  const { db } = require("zevbackv2");
-  console.log("req.params", req.params);
-  var kholboltuud = db.kholboltuud;
-  var tukhainBaaziinKholbolt = kholboltuud.find(
-    (a) => a.baiguullagiinId == req.params.baiguullagiinId
-  );
-  console.log("tukhainBaaziinKholbolt", tukhainBaaziinKholbolt);
-  const data = await Asuult(tukhainBaaziinKholbolt).findOne({
-    _id: req.params.id,
-  });
-  console.log("data", data);
-  res.send(data);
+  try {
+    const { db } = require("zevbackv2");
+    console.log("req.params", req.params);
+    var kholboltuud = db.kholboltuud;
+    var tukhainBaaziinKholbolt = kholboltuud.find(
+      (a) => a.baiguullagiinId == req.params.baiguullagiinId
+    );
+    console.log("tukhainBaaziinKholbolt", tukhainBaaziinKholbolt);
+    const data = await Asuult(tukhainBaaziinKholbolt).findOne({
+      _id: req.params.id,
+    });
+    console.log("data", data);
+    res.send(data);
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
