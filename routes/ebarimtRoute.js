81da4c5aabd966d483a88df9ff7f2e0d3cc5ac73
Tsend16 <d.is11d102@gmail.com>
2023-03-01 10:34:17 +0800

    2 urjigdsen kod awch xaylaa

--- routes/ebarimtRoute.js
+++ routes/ebarimtRoute.js
@@ -77,26 +77,6 @@ async function ebarimtDuudya(ugugdul, onFinish, next) {
     var url = process.env.EBARIMT_IP + "/put";
     if (ugugdul.barilgiinId)
       url = url + "?lib=" + ugugdul.barilgiinId.toString();
-    console.log("ebarimt put ==>", url);
-    request.post(
-      url,
-      { json: true, body: { data: ugugdul } },
-      (err, res1, body) => {
-        if (err) next(err);
-        else {
-          console.log("ebarimt body", body);
-          onFinish(body);
-        }
-      }
-    );
-  } catch (aldaa) {
-    next(aldaa);
-  }
-  try {
-    const data = new TextEncoder().encode(JSON.stringify(ugugdul));
-    var url = process.env.EBARIMT_IP + "/put";
-    if (ugugdul.barilgiinId)
-      url = url + "?lib=" + ugugdul.barilgiinId.toString();
     request.post(
       url,
       { json: true, body: { data: ugugdul } },
