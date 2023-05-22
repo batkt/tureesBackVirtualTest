a592bd7507164126dcd1b01b2d05600c9e9e363c
LBayandalai <bayanaa.work.tabs@gmail.com>
2023-05-22 17:58:53 +0800

    zasvar

--- models/geree.js
+++ models/geree.js
@@ -81,6 +81,7 @@ const gereeSchema = new Schema(
     baritsaaAvakhKhugatsaa: Number,
     baiguullagiinId: String,
     baiguullagiinNer: String,
+    actiinZagvariinId: String,
     barilgiinId: String,
     gereeniiZagvariinId: String,
     tulukhUdur: [String],
