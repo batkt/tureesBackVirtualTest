05191b92870772401d5f479bd9e57187b1f3c4cc
Hosoo <a_xocoo@yahoo.com>
2023-06-29 15:33:52 +0800

    pariking

--- routes/parkingRoute.js
+++ routes/parkingRoute.js
@@ -167,7 +167,7 @@ router.post(
                                     0,
                                 ],
                             },
-                        } : 0,
+                        } : {},
                         niitDun : {
                             $sum: {$ifNull: ["$niitDun", 0]}
                         }
