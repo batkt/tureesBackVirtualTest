799b8ec400bffd19350b5e5df99d0fbd322c9453
Tsend16 <d.is11d102@gmail.com>
2023-08-02 15:04:41 +0800

    parking

--- routes/parkingRoute.js
+++ routes/parkingRoute.js
@@ -142,31 +142,31 @@ router.post("/zogsoolSdkService", tokenShalgakh, async (req, res, next) => {
           body: `<div>
           <div style="display:flex; flex-direction:row; justify-content:space-between">
             <p style="width:maxContent; text-align:left">Машин:</p>
-            <p style="width:maxContent; text-align:right; color: #C0C0C0">${
+            <p style="width:maxContent; text-align:right; color: #999999">${
               uilchluulegch.mashiniiDugaar
             }</p>
           </div>
           <div style="display:flex; flex-direction:row; justify-content:space-between">
             <p style="width:maxContent; text-align:left">Орсон:</p>
-            <p style="width:maxContent; text-align:right; color: #C0C0C0">${moment(
+            <p style="width:maxContent; text-align:right; color: #999999">${moment(
               uilchluulegch.tuukh[0].tsagiinTuukh[0].orsonTsag
             ).format("YYYY/MM/DD HH:mm:ss")}</p>
           </div>
           <div style="display:flex; flex-direction:row; justify-content:space-between">
             <p style="width:maxContent; text-align:left">Гарсан:</p>
-            <p style="width:maxContent; text-align:right; color: #C0C0C0">${moment(
+            <p style="width:maxContent; text-align:right; color: #999999">${moment(
               uilchluulegch.tuukh[0].tsagiinTuukh[0].garsanTsag
             ).format("YYYY/MM/DD HH:mm:ss")}</p>
           </div>
           <div style="display:flex; flex-direction:row; justify-content:space-between">
             <p style="width:maxContent; text-align:left">Хугацаа:</p>
-            <p style="width:maxContent; text-align:right; color: #C0C0C0">${
+            <p style="width:maxContent; text-align:right; color: #999999">${
               uilchluulegch.tuukh[0].niitKhugatsaa
             } мин</p>
           </div>
           <div style="display:flex; flex-direction:row; justify-content:space-between">
             <p style="width:maxContent; text-align:left">Дүн:</p>
-            <p style="width:maxContent; text-align:right; color: #C0C0C0; font-weigth: bold">${
+            <p style="width:maxContent; text-align:right; color: #999999; font-weight: bold">${
               uilchluulegch.tuukh[0].tulukhDun
             } ₮</p>
           </div>
