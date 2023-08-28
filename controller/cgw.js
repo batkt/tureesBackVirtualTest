7714b899bd80ead5cf11a2863f2e447d912a8f35
Tsend16 <d.is11d102@gmail.com>
2023-08-28 17:54:27 +0800

    zasaw

--- controller/cgw.js
+++ controller/cgw.js
@@ -801,6 +801,19 @@ exports.bankniiKhuulgaTatyaOirkhon = asyncHandler(async () => {
                         x.barilgiinId = dans.barilgiinId;
                       });
                       if (guilgeenuud) {
+                        var ustgakhJagsaalt = [];
+                        for await (const item of guilgeenuud) {
+                          var guilgee = await BankniiGuilgee(kholbolt).findOne({
+                            NtryRef: item.NtryRef,
+                            barilgiinId: dans.barilgiinId,
+                          });
+                          if (guilgee) ustgakhJagsaalt.push(item);
+                        }
+                        if (ustgakhJagsaalt) {
+                          guilgeenuud = guilgeenuud.filter(
+                            (el) => !ustgakhJagsaalt.includes(el)
+                          );
+                        }
                       }
                       BankniiGuilgee(kholbolt)
                         .insertMany(guilgeenuud)
