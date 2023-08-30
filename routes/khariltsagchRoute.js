c56f17e6c54c98ffde61712281ba5b147c2904f2
Tsend16 <d.is11d102@gmail.com>
2023-08-30 10:26:02 +0800

    geree.gereeniiDugaar

--- routes/khariltsagchRoute.js
+++ routes/khariltsagchRoute.js
@@ -179,7 +179,7 @@ router
       if (davkhar) {
         matchQuery["geree.davkhar"] = davkhar;
       }
-      matchQuery["geree"] = { $exists: true };
+      matchQuery["geree.gereeniiDugaar"] = { $exists: true };
       matchQuery["geree.tuluv"] = { $nin: [-1] };
       if (matchQuery)
         query.push({
