8394fd71ac72276e99d79fc935063a45ceabc21e
Bat-Erdene <123363316+hanargaghd@users.noreply.github.com>
2023-11-15 10:04:25 +0800

    update

--- routes/nekhemjlekhiinZagvarRoute.js
+++ routes/nekhemjlekhiinZagvarRoute.js
@@ -19,16 +19,17 @@ router
     try {
       const turul = req.body.turul;
       const baiguullagiinId = req.body.baiguullagiinId;
+      const barilgiinId = req.body.barilgiinId;
       if (!req.file) {
         throw new aldaa("Excel файл дахин оруулна уу.");
       }
       const excelFile = req.file;
-      const savePath = `./excel/${turul}/${baiguullagiinId}/`;
+      const savePath = `./excel/${turul}/${baiguullagiinId}/${barilgiinId}`;
       if (!fs.existsSync(savePath)) {
         fs.mkdirSync(savePath, { recursive: true });
       }
       const garaasNershil = req.body.excelNer;
-      const excelFileName = `${turul}${baiguullagiinId}_${garaasNershil}.xlsx`;
+      const excelFileName = `${turul}${barilgiinId}_${garaasNershil}.xlsx`;
       fs.writeFile(`${savePath}${excelFileName}`, excelFile.buffer, (err) => {
         if (err) {
           throw new aldaa(err);
@@ -40,13 +41,94 @@ router
     }
   });
 
+const undsenTalbaruud = [
+  { ner: "Овог", talbar: "<ovog>" },
+  { ner: "Нэр", talbar: "<ner>" },
+  { ner: "Гэрээний огноо", talbar: "<gereeniiOgnoo>" },
+  { ner: "Төрөл", talbar: "<turul>" },
+  { ner: "Регистр", talbar: "<register>" },
+  { ner: "Албан тушаал", talbar: "<albanTushaal>" },
+  { ner: "Захиралын овог", talbar: "<zakhirliinOvog>" },
+  { ner: "Захиралын нэр", talbar: "<zakhirliinNer>" },
+  { ner: "Утас", talbar: "<utas>" },
+  { ner: "Хаяг", talbar: "<khayag>" },
+  { ner: "Гэрээний дугаар", talbar: "<gereeniiDugaar>" },
+  { ner: "Гарын үсэг", talbar: "<gariinUseg>" },
+  { ner: "Тамга", talbar: "<tamga>" },
+];
+
+const khugatsaaniiTalbaruud = [
+  { ner: "Хугацаа", talbar: "<khugatsaa>" },
+  { ner: "Эхлэх он", talbar: "<ekhlekhOn>" },
+  { ner: "Эхлэх сар", talbar: "<ekhelkhSar>" },
+  { ner: "Эхлэх өдөр", talbar: "<ekhlekhUdur>" },
+  { ner: "Дуусах он", talbar: "<duusakhOn>" },
+  { ner: "Дуусах сар", talbar: "duusakhSar" },
+  { ner: "Дуусах өдөр", talbar: "<duusakhUdur>" },
+];
+
+const talbainiiTalbaruud = [
+  { ner: "Талбайн дугаар", talbar: "<talbainDugaar>" },
+  { ner: "Талбайн нэгж үнэ", talbar: "<talbainNegjUne>" },
+  { ner: "Талбайн нэгж үнэ үсгээр", talbar: "<talbainNegjUneUsgeer>" },
+  { ner: "Талбайн нийт үнэ", talbar: "<talbainNiitUne>" },
+  { ner: "Талбайн нийт үнэ/Нөатгүй/", talbar: "<talbainNiitUneNuatgui>" },
+  { ner: "Талбайн нийт үнэ/Нөат/", talbar: "<talbainNiitUneNuat>" },
+  { ner: "Талбайн нийт үнэ үсгээр", talbar: "<talbainNiitUneUsgeer>" },
+  { ner: "Талбайн хэмжээ", talbar: "<talbainKhemjee>" },
+  { ner: "Түрээсийн талбайн давхар", talbar: "<davkhar>" },
+  { ner: "Зардлын дүн", talbar: "<zardliinDun>" },
+  { ner: "Зориулалт", talbar: "<zoriulalt>" },
+];
+
+const baritsaaniiTalbaruud = [
+  { ner: "Барьцаа авах дүн", talbar: "<baritsaaAvakhDun>" },
+  {
+    ner: "Барьцаа байршуулах хугацаа",
+    talbar: "baritsaaBairshuulakhKhugatsaa",
+  },
+];
+
+const tulburiinTalbaruud = [
+  { ner: "Хөнгөлөх хугацаа", talbar: "<khungulukhKhugatsaa>" },
+  { ner: "Сарын түрээс", talbar: "<sariinTurees>" },
+  { ner: "Мөнгөн дүн үсгээр", talbar: "<mungunDunUsgeer>" },
+  { ner: "Энэ сард төлөх дүн", talbar: "<eneSardTulukhDun>" },
+  { ner: "Нийт үлдэгдэл", talbar: "<niitUldegdel>" },
+  { ner: "Нийт үлдэгдэл/Нөатгүй/", talbar: "<niitUldegdelNuatgui>" },
+  { ner: "Нийт үлдэгдэл/Нөат/", talbar: "<niitUldegdelNuat>" },
+  { ner: "Алдангын үлдэгдэл", talbar: "<aldangiinUldegdel>" },
+  { ner: "Түрээсийн хөнгөлөлт", talbar: "<khungulult>" },
+];
+
+const nekhemjlekhiinTalbaruud = [
+  { ner: "Нэхэмжлэхийн сар", talbar: "<sar>" },
+  { ner: "Данс", talbar: "<dans>" },
+  { ner: "Дансны нэр", talbar: "<dansniiNer>" },
+  { ner: "Банк", talbar: "<bank>" },
+  { ner: "Хэвлэсэн огноо", talbar: "<khevlesenOgnoo>" },
+  { ner: "Нэхэмжлэхийн дугаар", talbar: "<nekhemjlekhiinDugaar>" },
+  { ner: "Өмнөх хуримтлагдсан өр төлбөр", talbar: "<umnukhSariinUrTulbur>" },
+  { ner: "Энэ сард төлөх үсгээр", talbar: "<eneSardTulukhUsgeer>" },
+  { ner: "Нийт үлдэгдэл үсгээр", talbar: "<niitUldegdelUsgeer>" },
+];
+
+const nekhemjlekhiinNemelt = [
+  { ner: "Дугаар", talbar: "<№>" },
+  { ner: "Тайлбар", talbar: "<nemeltNekhemjlekh.tailbar>" },
+  { ner: "Төлөх дүн", talbar: "<nemeltNekhemjlekh.tulukhDun>" },
+  { ner: "Огноо", talbar: "<nemeltNekhemjlekh.ognoo>" },
+  { ner: "Бусад авлагын мөр", talbar: "<nemeltNekhemjlekh>" },
+];
+
 router.route("/excelZagvarUstgaya").post(tokenShalgakh, (req, res, next) => {
   try {
     const turul = req.body.turul;
     const baiguullagiinId = req.body.baiguullagiinId;
+    const barilgiinId = req.body.barilgiinId;
     const excelNer = req.body.excelNer;
 
-    const filePath = `./excel/${turul}/${baiguullagiinId}/${turul}${baiguullagiinId}_${excelNer}.xlsx`;
+    const filePath = `./excel/${turul}/${baiguullagiinId}/${barilgiinId}/${turul}${barilgiinId}_${excelNer}.xlsx`;
 
     fs.unlink(filePath, (err) => {
       if (err) {
@@ -136,6 +218,74 @@ function textSolyo(text, body) {
   return butsaakh;
 }
 
+router.route("/excelTalbaruud").get(tokenShalgakh, async (req, res, next) => {
+  try {
+    const ashiglaltiinZardluud = req.params.ashiglaltiinZardluud;
+    var songokhTalbaruud = [];
+    songokhTalbaruud.concat(
+      undsenTalbaruud,
+      khugatsaaniiTalbaruud,
+      talbainiiTalbaruud,
+      baritsaaniiTalbaruud,
+      tulburiinTalbaruud,
+      nekhemjlekhiinNemelt,
+      nekhemjlekhiinTalbaruud
+    );
+    if (!!ashiglaltiinZardluud && ashiglaltiinZardluud.length > 0) {
+      ashiglaltiinZardluud.map((a) => {
+        songokhTalbaruud.push({
+          ner: `${a.ner}.Дүн`,
+          talbar: `${a.ner}.tulukhDun`,
+        });
+        songokhTalbaruud.push({
+          ner: `${a.ner}.Хэмжих нэгж`,
+          talbar: `${a.ner}.khemjikhNegj`,
+        });
+        songokhTalbaruud.push({
+          ner: `${a.ner}.Тариф`,
+          talbar: `${a.ner}.tariff`,
+        });
+        songokhTalbaruud.push({
+          ner: `${a.ner}.Нэгж`,
+          talbar: `${a.ner}.negj`,
+        });
+        if (a.turul == "кВт" || a.turul == "1м3") {
+          songokhTalbaruud.push({
+            ner: `${a.ner}.Өмнөх заалт`,
+            talbar: `${a.ner}.umnukhZaalt`,
+          });
+          songokhTalbaruud.push({
+            ner: `${a.ner}.Сүүлийн заалт`,
+            talbar: `${a.ner}.suuliinZaalt`,
+          });
+        } else {
+          songokhTalbaruud.push({
+            ner: `${a.ner}.Хөнгөлөлт`,
+            talbar: `${a.ner}.khungulult`,
+          });
+        }
+      });
+      songokhTalbaruud.push({
+        ner: `Нийт ашиглалтын зардал`,
+        talbar: `niitZardliinDun`,
+      });
+
+      songokhTalbaruud.push({
+        ner: `Нийт ашиглалтын зардал/Нөатгүй/`,
+        talbar: `niitZardliinNuatguiDun`,
+      });
+
+      songokhTalbaruud.push({
+        ner: `Нөат (10%)`,
+        talbar: `niitZardliinNuatiinDun`,
+      });
+    }
+    res.send(songokhTalbaruud);
+  } catch (err) {
+    next(err);
+  }
+});
+
 function numberToWords(number, option, bukhel, butarhai) {
   const { fixed, suffix } = option;
   let resValue = "";
@@ -151,86 +301,6 @@ function numberToWords(number, option, bukhel, butarhai) {
   return resValue;
 }
 
-const undsenTalbaruud = [
-  { ner: "Овог", talbar: "<ovog>" },
-  { ner: "Нэр", talbar: "<ner>" },
-  { ner: "Гэрээний огноо", talbar: "<gereeniiOgnoo>" },
-  { ner: "Төрөл", talbar: "<turul>" },
-  { ner: "Регистр", talbar: "<register>" },
-  { ner: "Албан тушаал", talbar: "<albanTushaal>" },
-  { ner: "Захиралын овог", talbar: "<zakhirliinOvog>" },
-  { ner: "Захиралын нэр", talbar: "<zakhirliinNer>" },
-  { ner: "Утас", talbar: "<utas>" },
-  { ner: "Хаяг", talbar: "<khayag>" },
-  { ner: "Гэрээний дугаар", talbar: "<gereeniiDugaar>" },
-  { ner: "Гарын үсэг", talbar: "<gariinUseg>" },
-  { ner: "Тамга", talbar: "<tamga>" },
-];
-
-const khugatsaaniiTalbaruud = [
-  { ner: "Хугацаа", talbar: "<khugatsaa>" },
-  { ner: "Эхлэх он", talbar: "<ekhlekhOn>" },
-  { ner: "Эхлэх сар", talbar: "<ekhelkhSar>" },
-  { ner: "Эхлэх өдөр", talbar: "<ekhlekhUdur>" },
-  { ner: "Дуусах он", talbar: "<duusakhOn>" },
-  { ner: "Дуусах сар", talbar: "duusakhSar" },
-  { ner: "Дуусах өдөр", talbar: "<duusakhUdur>" },
-];
-
-const talbainiiTalbaruud = [
-  { ner: "Талбайн дугаар", talbar: "<talbainDugaar>" },
-  { ner: "Талбайн нэгж үнэ", talbar: "<talbainNegjUne>" },
-  { ner: "Талбайн нэгж үнэ үсгээр", talbar: "<talbainNegjUneUsgeer>" },
-  { ner: "Талбайн нийт үнэ", talbar: "<talbainNiitUne>" },
-  { ner: "Талбайн нийт үнэ/Нөатгүй/", talbar: "<talbainNiitUneNuatgui>" },
-  { ner: "Талбайн нийт үнэ/Нөат/", talbar: "<talbainNiitUneNuat>" },
-  { ner: "Талбайн нийт үнэ үсгээр", talbar: "<talbainNiitUneUsgeer>" },
-  { ner: "Талбайн хэмжээ", talbar: "<talbainKhemjee>" },
-  { ner: "Түрээсийн талбайн давхар", talbar: "<davkhar>" },
-  { ner: "Зардлын дүн", talbar: "<zardliinDun>" },
-  { ner: "Зориулалт", talbar: "<zoriulalt>" },
-];
-
-const baritsaaniiTalbaruud = [
-  { ner: "Барьцаа авах дүн", talbar: "<baritsaaAvakhDun>" },
-  {
-    ner: "Барьцаа байршуулах хугацаа",
-    talbar: "baritsaaBairshuulakhKhugatsaa",
-  },
-];
-
-const tulburiinTalbaruud = [
-  { ner: "Хөнгөлөх хугацаа", talbar: "<khungulukhKhugatsaa>" },
-  { ner: "Сарын түрээс", talbar: "<sariinTurees>" },
-  { ner: "Мөнгөн дүн үсгээр", talbar: "<mungunDunUsgeer>" },
-  { ner: "Энэ сард төлөх дүн", talbar: "<eneSardTulukhDun>" },
-  { ner: "Нийт үлдэгдэл", talbar: "<niitUldegdel>" },
-  { ner: "Нийт үлдэгдэл/Нөатгүй/", talbar: "<niitUldegdelNuatgui>" },
-  { ner: "Нийт үлдэгдэл/Нөат/", talbar: "<niitUldegdelNuat>" },
-  { ner: "Алдангын үлдэгдэл", talbar: "<aldangiinUldegdel>" },
-  { ner: "Түрээсийн хөнгөлөлт", talbar: "<khungulult>" },
-];
-
-const nekhemjlekhiinTalbaruud = [
-  { ner: "Нэхэмжлэхийн сар", talbar: "<sar>" },
-  { ner: "Данс", talbar: "<dans>" },
-  { ner: "Дансны нэр", talbar: "<dansniiNer>" },
-  { ner: "Банк", talbar: "<bank>" },
-  { ner: "Хэвлэсэн огноо", talbar: "<khevlesenOgnoo>" },
-  { ner: "Нэхэмжлэхийн дугаар", talbar: "<nekhemjlekhiinDugaar>" },
-  { ner: "Өмнөх хуримтлагдсан өр төлбөр", talbar: "<umnukhSariinUrTulbur>" },
-  { ner: "Энэ сард төлөх үсгээр", talbar: "<eneSardTulukhUsgeer>" },
-  { ner: "Нийт үлдэгдэл үсгээр", talbar: "<niitUldegdelUsgeer>" },
-];
-
-const nekhemjlekhiinNemelt = [
-  { ner: "Дугаар", talbar: "<№>" },
-  { ner: "Тайлбар", talbar: "<nemeltNekhemjlekh.tailbar>" },
-  { ner: "Төлөх дүн", talbar: "<nemeltNekhemjlekh.tulukhDun>" },
-  { ner: "Огноо", talbar: "<nemeltNekhemjlekh.ognoo>" },
-  { ner: "Бусад авлагын мөр", talbar: "<nemeltNekhemjlekh>" },
-];
-
 router
   .route("/excelZagvarTatya")
   .post(tokenShalgakh, async (req, res, next) => {
@@ -238,10 +308,11 @@ router
     const tulugch = Array.isArray(req.body.nekhemjlekhiinJagsaalt)
       ? req.body.nekhemjlekhiinJagsaalt[0]
       : req.body.nekhemjlekhiinJagsaalt;
+    const barilgiinId = req.body.barilgiinId;
     const turul = "nekhemjlel";
     const ashiglaltiinZardluud = req.body.ashiglaltiinZardluud;
     const garaasNershil = req.body.excelNer;
-    const savePath = `./excel/${turul}/${baiguullagiinId}/${turul}${baiguullagiinId}_${garaasNershil}.xlsx`;
+    const savePath = `./excel/${turul}/${baiguullagiinId}/${barilgiinId}/${turul}${barilgiinId}_${garaasNershil}.xlsx`;
     const workbook = new ExcelJS.Workbook();
     try {
       await workbook.xlsx.readFile(savePath);
@@ -336,9 +407,10 @@ router
   .route("/excelZagvarKharya")
   .post(tokenShalgakh, async (req, res, next) => {
     const baiguullagiinId = req.body.baiguullagiinId;
+    const barilgiinId = req.body.barilgiinId;
     const turul = "nekhemjlel";
     const fileNer = req.body.excelNer;
-    const zam = `./excel/${turul}/${baiguullagiinId}/${turul}${baiguullagiinId}_${fileNer}.xlsx`;
+    const zam = `./excel/${turul}/${baiguullagiinId}/${barilgiinId}/${turul}${barilgiinId}_${fileNer}.xlsx`;
     try {
       const workbook = XLSX.readFile(zam);
       const sheetName = workbook.SheetNames[0];
