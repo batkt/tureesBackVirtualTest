17babf010f621e83a01179661cd02e7d5ba149ab
Tsend16 <d.is11d102@gmail.com>
2024-01-08 15:17:08 +0800

    3sec tutamd

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ cron.schedule(
 );
 
 cron.schedule(
-  "*/2 * * * * * ",
+  "*/3 * * * * * ",
   function () {
     // console.log("xuulga tatlaa", new Date());
     cgw.bankniiKhuulgaTatyaOirkhon();
