d80528b4a64a157f38cc8aa12f86bc04978a8cc5
Tsend16 <d.is11d102@gmail.com>
2024-02-26 11:10:10 +0800

    zasaw

--- controller/ajiltan.js
+++ controller/ajiltan.js
@@ -257,7 +257,7 @@ exports.tokenoorAjiltanAvya = asyncHandler(async (req, res, next) => {
 exports.zochiniiTokenAvya = asyncHandler(async (req, res, next) => {
   try {
     const { db } = require("zevbackv2");
-    var zochin = new Ajiltan(db.erunkhiiKholbolt);
+    var zochin = new Ajiltan(db.erunkhiiKholbolt)();
     res.send(zochin.zochinTokenUusgye(req.params.baiguullagiinId));
   } catch (error) {
     next(error);
