5b83914a85a7448507399907b7759a3aa494a03e
Tsend16 <d.is11d102@gmail.com>
2024-02-28 09:39:01 +0800

    zasaw

--- routes/parkingRoute.js
+++ routes/parkingRoute.js
@@ -885,7 +885,7 @@ router.get("/v1/search_car/:plate_number", async (req, res, next) => {
     message = "Машины мэдээлэл олдсонгүй!";
     success = false;
   }
-  if (!!freeze && !!oldsonMashin) {
+  if ((!!freeze || localEsekh) && !!oldsonMashin) {
     await Uilchluulegch(tukhainKholbolt).updateOne(
       { _id: oldsonMashin._id },
       {
