1e5b65042a00c09a6fbb4f2c7716e5bf75755626
Tsend16 <d.is11d102@gmail.com>
2024-04-10 10:41:45 +0800

    zasaw

--- routes/parkingRoute.js
+++ routes/parkingRoute.js
@@ -1587,6 +1587,14 @@ router.route("/v1/kioskPay").post(tokenShalgakh, async (req, res, next) => {
         if (bodsonDun == req.body.paid_amount) {
           if (!!tukhainObject.tuukh[0]?.tsagiinTuukh[0]?.garsanTsag) {
             set["tuukh.$[t].tuluv"] = 1;
+            if (!!tukhainObject.tuukh[0]?.tsagiinTuukh[0]?.garsanKhaalga) {
+              const io = req.app.get("socketio");
+              io.emit(`zogsool${tukhainObject.baiguullagiinId}`, {
+                khaalgaTurul: "oroh",
+                cameraIP:
+                  tukhainObject.tuukh[0]?.tsagiinTuukh[0]?.garsanKhaalga,
+              });
+            }
           }
           set["tuukh.$[t].burtgesenAjiltaniiId"] = req.body.ajiltniiId;
           set["tuukh.$[t].burtgesenAjiltaniiNer"] = req.body.ajiltniiNer;
