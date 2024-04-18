04d8d27747fc2b9b686e10d9fb3c863693310bcd
Tsend16 <d.is11d102@gmail.com>
2024-04-18 11:44:14 +0800

    zasaw

--- routes/ebarimtRoute.js
+++ routes/ebarimtRoute.js
@@ -802,7 +802,7 @@ router.post("/ebarimtButsaaya", tokenShalgakh, async (req, res, next) => {
             });
         }
         console.log("duuslaa", d);
-        res.send(d);
+        res.send("Amjilttai");
       },
       next,
       ebarimtShine
