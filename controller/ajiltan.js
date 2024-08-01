514ee3bfac89c5803897cdfd8991a9e59f2fe5ac
Tsend16 <d.is11d102@gmail.com>
2024-08-01 12:44:54 +0800

    butsaaaw

--- controller/ajiltan.js
+++ controller/ajiltan.js
@@ -792,9 +792,6 @@ exports.orlogiinMsgIlgeeye = asyncHandler(
                   $gte: ekhlekhOgnoo,
                   $lte: duusakhOgnoo,
                 },
-                "tuukh.tuluv": {
-                  $in: [1, 2],
-                },
               },
             },
             {
