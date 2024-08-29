505b2e75f8c912e58de1f19afee6503606c6b765
batkt <168392649+batkt@users.noreply.github.com>
2024-08-30 00:17:23 +0800

    ss

--- routes/tasalbarRoute.js
+++ routes/tasalbarRoute.js
@@ -264,11 +264,15 @@ router
       doc.text("Hello world BATAA!", 10, 10);
       doc.save(temPath + "\\tasalbarKhevlekh\\khevlekh.pdf");
 
-      // const options = {
-      //   printer: "ZKP8008",
-      // };
-      // pdfPrint.getDefaultPrinter().then(console.log);
-      // pdfPrint.print(temPath + "\\tasalbarKhevlekh\\khevlekh.pdf", options).then(console.log);
+      console.log("---------5-------------->>>>"+temPath);
+
+      const options = {
+        printer: "ZKP8008",
+      };
+      pdfPrint.getDefaultPrinter().then(console.log);
+      console.log("----------8------------->>>>"+temPath);
+      pdfPrint.print(temPath + "\\tasalbarKhevlekh\\khevlekh.pdf", options).then(console.log);
+      console.log("----------9------------->>>>"+temPath);
       res.send("test");
     } catch (error) {
       next(error);
