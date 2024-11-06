18d888ff5830714e136231496d3857ac237cc5f7
batkt <168392649+batkt@users.noreply.github.com>
2024-11-06 15:29:11 +0800

    lt

--- controller/tulbur.js
+++ controller/tulbur.js
@@ -860,7 +860,7 @@ exports.uldegdelBodyo = asyncHandler(async (req, res, next) => {
     {
       $match: {
         "avlaga.guilgeenuud.ognoo": {
-          $lte: new Date(),
+          $lt: new Date(moment(moment().endOf("month").format("YYYY-MM-DD 23:59:59"))),
         },
         "avlaga.guilgeenuud.turul": {
           $nin: ["baritsaa"],
