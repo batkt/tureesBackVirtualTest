0058cc549123d92fb3c759230b3b469d0dd57f26
batkt <168392649+batkt@users.noreply.github.com>
2024-11-20 23:52:48 +0800

    a

--- models/ajiltan.js
+++ models/ajiltan.js
@@ -78,7 +78,8 @@ ajiltanSchema.methods.tokenUusgeye = function (duusakhOgnoo, salbaruud = null) {
         this.baiguullagiinId == "6698c657c26994f4e0f8de62" ||
         this.baiguullagiinId == "6731b43bc23730ac1908da2d" ||
         this.baiguullagiinId == "6115f350b35689cdbf1b9da3" ||
-        this.baiguullagiinId == "670f3437b41a478195dd3d4b"
+        this.baiguullagiinId == "670f3437b41a478195dd3d4b" ||
+        this.baiguullagiinId == "66d90703344b0b96a65ef1a4"
           ? "7d"
           : "12h",
     }
