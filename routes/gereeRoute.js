9d6ed137652f40985bdde32e91906db52f0c8ce3
batkt <168392649+batkt@users.noreply.github.com>
2024-11-22 07:29:23 +0800

    ObjectId

--- routes/gereeRoute.js
+++ routes/gereeRoute.js
@@ -507,13 +507,14 @@ router
   .route("/gereeZasya")
   .post(tokenShalgakh, gereeZasakhShalguur, async (req, res, next) => {
     try {
+      var ObjectId = require("mongodb").ObjectId;
       var geree = new Geree(req.body.tukhainBaaziinKholbolt)(req.body);
-      var gereeOld = await Geree(req.body.tukhainBaaziinKholbolt).findById(geree._id).select("+avlaga");
+      var gereeOld = await Geree(req.body.tukhainBaaziinKholbolt).findOne({_id: new ObjectId(geree._id) }).select("+avlaga");
       if(gereeOld?.avlaga?.guilgeenuud?.length > 0)
       {
         if(geree?.avlaga?.guilgeenuud?.length > 0)
         {
-          var filterTulsunDun = gereeOld?.avlaga?.guilgeenuud?.filter((a) => a.ekhniiUldegdelEsekh || a.tulsunDun > 0 || a.tulsunAldangi > 0 || a.khyamdral > 0 || a.suuliinZaalt > 0 || a.umnukhZaalt);
+          var filterTulsunDun = gereeOld?.avlaga?.guilgeenuud?.filter((a) => !!a.guilgeeKhiisenAjiltniiNer || !!a.guilgeeKhiisenAjiltniiId || a.ekhniiUldegdelEsekh || a.tulsunDun > 0 || a.tulsunAldangi > 0 || a.khyamdral > 0 || a.suuliinZaalt > 0 || a.umnukhZaalt);
           if(filterTulsunDun?.length > 0)
             geree?.avlaga?.guilgeenuud.push(...filterTulsunDun);
         }
