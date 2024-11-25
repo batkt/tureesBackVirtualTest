a4aedd22219b0eeb11408d31e62712670ba86640
batkt <168392649+batkt@users.noreply.github.com>
2024-11-25 16:19:39 +0800

    я

--- routes/gereeRoute.js
+++ routes/gereeRoute.js
@@ -1446,7 +1446,7 @@ router
                 if (!!x.zardluud && x.zardluud.length > 0) {
                   x.zardluud.forEach((zardal) => {
                     zardal.tailbar = zardal._id.tailbar;
-                    if (zardal.tailbar == "Түрээс")
+                    if (zardal.tailbar == "Хөнгөлөлт")
                       x.khungulult = zardal.khungulult;
                   });
                 }
