0bc2c00a52ef9f6d28734104c3ae081b8e7b746f
batkt <168392649+batkt@users.noreply.github.com>
2024-11-27 23:30:49 +0800

    ss

--- controller/cgw.js
+++ controller/cgw.js
@@ -1224,6 +1224,23 @@ exports.bankniiKhuulgaTatajKhadgalya = asyncHandler(async (req, res, next) => {
                   x.baiguullagiinId = dans.baiguullagiinId;
                   x.barilgiinId = dans.barilgiinId;
                 });
+                if (guilgeenuud) {
+                  var ustgakhJagsaalt = [];
+                  for await (const item of guilgeenuud) {
+                    var guilgee = await BankniiGuilgee(kholbolt).findOne({
+                      requestId: item.requestId,
+                      recNum: item.recNum,
+                      dansniiDugaar: item.dansniiDugaar,
+                      barilgiinId: dans.barilgiinId,
+                    });
+                    if (guilgee) ustgakhJagsaalt.push(item);
+                  }
+                  if (!!ustgakhJagsaalt) {
+                    guilgeenuud = guilgeenuud.filter(
+                      (el) => !ustgakhJagsaalt.includes(el)
+                    );
+                  }
+                }
                 BankniiGuilgee(kholbolt)
                     .insertMany(guilgeenuud)
                     .then((result) => {
