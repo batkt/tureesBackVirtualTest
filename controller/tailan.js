3676370bc0461fb21726bfd5c1231aeb410bc97d
batkt <168392649+batkt@users.noreply.github.com>
2024-12-11 18:17:40 +0800

    zz

--- controller/tailan.js
+++ controller/tailan.js
@@ -1962,26 +1962,17 @@ exports.negtgelTailanAvya = asyncHandler(async (req, res, next) => {
         var tempJagsaaltAvlaga = a.avlaga;
         var indexTemp = 3;
         
-        var tempZardliinTurulFilter = a.avlaga.filter((o) => { return !!o.zardliinTurul});
-        // var guilgeeKhiisenFilter = a.avlaga.filter((o) => { return !!o.guilgeeKhiisenAjiltniiId});
-        a.avlaga = a.avlaga.filter((o) => { return !o.zardliinTurul }); // && !o.guilgeeKhiisenAjiltniiId
-
         a.avlaga?.forEach((b) => {
-          var filter = tempZardliinTurulFilter?.filter((e) => moment(e.ognoo).format("YYYY-MM") === moment(b.ognoo).format("YYYY-MM") && e.turul === "khuvaari");
-          if(filter?.length > 0 && b.turul === "khuvaari")
-            b.tulukhDun += filter?.reduce((a, b) => a + (b.tulukhDun || 0), 0);
-
-          var filterManagement = tempZardliinTurulFilter?.filter((e) => moment(e.ognoo).format("YYYY-MM") === moment(b.ognoo).format("YYYY-MM") && e.turul === "management");
-          if(filterManagement?.length > 0 && b.turul === "avlaga" && (b.tailbar === "Менежментийн төлбөр" || b.tailbar === "Менежментийн зардал" || b.tailbar === "Менежмент"))
-            b.tulukhDun += filterManagement?.reduce((a, b) => a + (b.tulukhDun || 0), 0);
-         
-          var filterDulaan = tempZardliinTurulFilter?.filter((e) => moment(e.ognoo).format("YYYY-MM") === moment(b.ognoo).format("YYYY-MM") && e.turul === "dulaan");
-          if(filterDulaan?.length > 0 && b.turul === "avlaga" && b.tailbar === "Дулаан")
-            b.tulukhDun += filterDulaan?.reduce((a, b) => a + (b.tulukhDun || 0), 0);
-
-          // var filterGuilgee = guilgeeKhiisenFilter?.filter((e) => moment(e.ognoo).format("YYYY-MM") === moment(b.ognoo).format("YYYY-MM") && e.turul === "avlaga" && e.tailbar === b.tailbar);
-          // if(filterGuilgee?.length > 0 && b.turul === "avlaga")
-          //   b.tulukhDun += filterGuilgee?.reduce((a, b) => a + (b.tulukhDun || 0), 0);
+          if(b.zardliinTurul === "management") 
+            b.tailbar = "Менежментийн зардал";
+          if(b.zardliinTurul === "dulaan") 
+            b.tailbar = "Дулаан";
+          if(b.zardliinTurul === "tsakhilgaan") 
+            b.tailbar = "Цахилгаан";
+          if(b.zardliinTurul === "khulaanUs") 
+            b.tailbar = "Халуун ус";
+          if(b.zardliinTurul === "khuitenUs") 
+            b.tailbar = "Хүйтэн ус";
 
           var tempkhungulult = tempJagsaaltAvlaga?.filter((e) => moment(e.ognoo).format("YYYY-MM") === moment(b.ognoo).format("YYYY-MM") && e.turul === "khungulult");
           if(b.turul !== "khuvaari" || (tempkhungulult?.length === 0 && b.turul === "khuvaari"))
