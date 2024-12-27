23ce07ca190d96f5fec9254db0ee411ac9dcbdd0
batkt <168392649+batkt@users.noreply.github.com>
2024-12-27 21:40:55 +0800

    cmd

--- routes/parkingRoute.js
+++ routes/parkingRoute.js
@@ -2278,4 +2278,29 @@ router.route("/mashinUpdate1").post(async (req, res, next) => {
   }
 });
 
+router.post("/zogsoolMobileSdk", tokenShalgakh, async (req, res, next) => {
+  try {
+    console.log("mashiniiDugaar ----------------->>" + req.body.mashiniiDugaar);
+    console.log("cameraIP ----------------->>" + req.body.cameraIP);
+    console.log("qpayObject ----------------->>" + JSON.stringify(req.body.qpayObject));
+    if(req.body.baiguullagiinId === "6715ef2ca5cefb3e54505428")
+    {
+      const io = req.app.get("socketio");
+      if(io)
+      {
+        io.emit(`qpayMobileSdk${req.body.baiguullagiinId}`, {
+          khaalgaTurul: "Гарах",
+          turul: "qpayMobile",
+          mashiniiDugaar: req.body.mashiniiDugaar,
+          cameraIP: req.body.cameraIP,
+          qpayObject: req.body.qpayObject,
+        });
+      }
+    }
+    res.send(req.body.mashiniiDugaar);
+  } catch (err) {
+    next(err);
+  }
+});
+
 module.exports = router;
