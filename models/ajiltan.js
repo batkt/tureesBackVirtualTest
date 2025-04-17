9b5a51eda350415e97b5e52758aca95568464f11
batkt <sw10d057@gmail.com>
2025-04-17 16:24:38 +0800

    astra 7d add

--- models/ajiltan.js
+++ models/ajiltan.js
@@ -82,7 +82,8 @@ ajiltanSchema.methods.tokenUusgeye = function (duusakhOgnoo, salbaruud = null) {
         this.baiguullagiinId == "6115f350b35689cdbf1b9da3" ||
         this.baiguullagiinId == "670f3437b41a478195dd3d4b" ||
         this.baiguullagiinId == "66d90703344b0b96a65ef1a4" ||
-        this.baiguullagiinId == "67dfebe55b92ee004ba43ad2"
+        this.baiguullagiinId == "67dfebe55b92ee004ba43ad2" ||
+        this.baiguullagiinId == "67b5a998b72a35321ebdbd87"
           ? "7d"
           : "12h",
     }
