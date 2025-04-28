a7df6e60cbe5ca14d719d5d08c99066db6933407
batkt <sw10d057@gmail.com>
2025-04-28 17:24:50 +0800

    26

--- index.js
+++ index.js
@@ -308,7 +308,7 @@ cron.schedule(
 );
 
 cron.schedule(
-  "20 * * * * ",
+  "26 * * * * ",
   function () {
     console.log("sarBuriinKhungulultBodoy ---", new Date());
     gereeRoute.sarBuriinKhungulultBodoy();
