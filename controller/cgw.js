07b2f678e622bacc6b534b0fe72fb8d1f282e6ac
batkt <sw10d057@gmail.com>
2025-05-23 01:14:04 +0800

    z shunu

--- controller/cgw.js
+++ controller/cgw.js
@@ -386,7 +386,7 @@ async function dansniiKhuulgaAvya(token, next, body) {
     const responseShunuEsekh = await instance.get("https://api.khanbank.com/v1/statements/corporate/state", { context }); 
     var url = "https://api.khanbank.com/v1/statements/" + (responseShunuEsekh?.body ? "corporate/" : "") + body.dansniiDugaar;
     if(body.record)
-      url = url + "/record?record=" + body.record;
+      url = url + (responseShunuEsekh?.body ? "/?record=" : "/record?record=") + body.record;
     console.log("url -------------------->>" + JSON.stringify(url));
     const response = await instance.get(url, { context });
     if (!response.body) {
