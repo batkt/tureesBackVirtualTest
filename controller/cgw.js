2529d7bfb4dce5e853d6478a406076a0b03e5df4
batkt <sw10d057@gmail.com>
2025-05-23 01:44:49 +0800

    token kheviin

--- controller/cgw.js
+++ controller/cgw.js
@@ -389,8 +389,8 @@ async function dansniiKhuulgaAvya(token, next, body) {
       const responseShunuEsekh = await instance.get("https://api.khanbank.com/v1/statements/corporate/state", { context }); 
       console.log("sojolj state -------------------->>" + JSON.stringify(responseShunuEsekh?.body));
       url = "https://api.khanbank.com/v1/statements/" + (responseShunuEsekh?.body ? "corporate/" : "") + body.dansniiDugaar;
-      // if(body.record)
-      //   url = url + (responseShunuEsekh?.body ? "/?record=" : "/record?record=") + body.record;
+      if(body.record)
+        url = url + (responseShunuEsekh?.body ? "/?record=" : "/record?record=") + body.record;
       console.log("sojolj url -------------------->>" + JSON.stringify(url));
     }
     else
