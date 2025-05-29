25df5aa12e4863ff39f6c8e4c80beda28723d3b3
batkt <sw10d057@gmail.com>
2025-05-29 16:02:09 +0800

    z

--- index.js
+++ index.js
@@ -319,18 +319,6 @@ cron.schedule(
   }
 );
 
-cron.schedule(
-  "*/1 * * * * * ",
-  function () {
-    console.log("testCloudMongodb --->");
-    zogsool.testCloudMongodb();
-  },
-  {
-    scheduled: true,
-    timezone: "Asia/Ulaanbaatar",
-  }
-);
-
 io.once("connection", (socket) => {
   console.log("connected");
   socket.on("disconnect", () => {
