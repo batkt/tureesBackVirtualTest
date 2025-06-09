7881b75a50221d0b3ffff6ad7485e59068322b99
batkt <sw10d057@gmail.com>
2025-06-09 22:58:56 +0800

    f

--- models/bankniiGuilgee.js
+++ models/bankniiGuilgee.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 mongoose.pluralize(null);
+mongoose.set('strictQuery', false);
 const bankniiGuilgeeSchema = new Schema(
   {
     id: String,
