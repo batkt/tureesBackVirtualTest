42625146759c6ee769fbe823a4e2179cbcc23552
batkt <sw10d057@gmail.com>
2025-06-10 16:09:17 +0800

    ognoo

--- controller/cgw.js
+++ controller/cgw.js
@@ -873,7 +873,7 @@ exports.bankniiKhuulgaTatajKhadgalya = asyncHandler(async (req, res, next) => {
                     firstDay = new Date(max.TxDt);
                   }
                   else
-                    firstDay = new Date(moment().startOf("month"));
+                    firstDay = new Date();
                   url = url + 
                   "?from=" +
                     firstDay.getFullYear() +
