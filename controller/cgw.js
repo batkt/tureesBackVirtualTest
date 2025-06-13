6e116bbb3c414bcf68996dbedb1b79958f35f886
batkt <sw10d057@gmail.com>
2025-06-13 15:47:31 +0800

    s

--- controller/cgw.js
+++ controller/cgw.js
@@ -2265,16 +2265,8 @@ exports.togloomiinTuvDavkhardsanShalgakh = asyncHandler(async (req, res, next) =
               if(togloom.tulbur > togloom._id?.niitDun)
               {
                 var data = await TogloomiinTuv(kholbolt).findById(togloom._id?.id);
-                var count = data.tulbur?.length;
-                var i = 1;
-                while (i <= count) {
-                  var tulburuud = data.tulbur?.reduce((a, b) => a + b?.dun, 0);
-                  if(tulburuud > data.niitDun)
-                  {
-                    data.niitTulbur?.shift();
-                    data.tulbur?.shift();
-                  }
-                }
+                data.niitTulbur?.shift();
+                data.tulbur?.shift();
                 TogloomiinTuv(kholbolt)
                   .findByIdAndUpdate({ _id: togloom._id?.id }, [
                     {
