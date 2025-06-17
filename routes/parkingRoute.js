66094449d4f2fddcf2aac69085d763ac18e1caa2
batkt <sw10d057@gmail.com>
2025-06-17 14:45:22 +0800

    z

--- routes/parkingRoute.js
+++ routes/parkingRoute.js
@@ -1076,11 +1076,11 @@ router.get("/v2/parking", async (req, res, next) => {
           for await (const zogsool of zogsooluud) {
             if (!!zogsool) {
               var dotorZogsool;
-              if (!!zogsool.dotorZogsooliinId) {
-                dotorZogsool = await Parking(kholbolt).findById(
-                  zogsool.dotorZogsooliinId
-                );
-              }
+              // if (!!zogsool.dotorZogsooliinId) {
+              //   dotorZogsool = await Parking(kholbolt).findById(
+              //     zogsool.dotorZogsooliinId
+              //   );
+              // }
               var xariu = await Uilchluulegch(kholbolt).aggregate([
                 {
                   $match: {
