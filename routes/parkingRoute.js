b7ee56fc124712eec78068873272008594fc7319
batkt <sw10d057@gmail.com>
2025-06-18 10:45:43 +0800

    log

--- routes/parkingRoute.js
+++ routes/parkingRoute.js
@@ -1159,21 +1159,21 @@ router.get("/v2/parking", async (req, res, next) => {
                   },
                 },
               ];
-              var xariu = await getAggregateUilchluulegch(kholbolt, zogsool.baiguullagiinId, zogsool.barilgiinId, query);
+              // var xariu = await getAggregateUilchluulegch(kholbolt, zogsool.baiguullagiinId, zogsool.barilgiinId, query);
               var parked = 0;
               var inside = {};
-              if (xariu && xariu.length > 0) {
-                if (!!dotorZogsool && !!zogsool.dotorZogsooliinId) {
-                  inside.total = dotorZogsool.too;
-                  inside.parked = xariu.find(
-                    (x) => x._id == dotorZogsool._id.toString()
-                  )?.too;
-                  if (!inside.parked) inside.parked = 0;
-                  parked = xariu.find((x) => x._id == zogsool._id.toString())?.too;
-                } else {
-                  parked = xariu[0].too;
-                }
-              }
+              // if (xariu && xariu.length > 0) {
+              //   if (!!dotorZogsool && !!zogsool.dotorZogsooliinId) {
+              //     inside.total = dotorZogsool.too;
+              //     inside.parked = xariu.find(
+              //       (x) => x._id == dotorZogsool._id.toString()
+              //     )?.too;
+              //     if (!inside.parked) inside.parked = 0;
+              //     parked = xariu.find((x) => x._id == zogsool._id.toString())?.too;
+              //   } else {
+              //     parked = xariu[0].too;
+              //   }
+              // }
               var slot = {
                 outside: {
                   total: zogsool.too,
