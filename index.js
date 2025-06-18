5352ddd2489dbeb5124cfc5f5351357c33b70f72
batkt <sw10d057@gmail.com>
2025-06-18 10:25:10 +0800

    z

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const passRoute = require("./routes/passRoute");
 const parkingRoute = require("./routes/parkingRoute");
 const eventRoute = require("./routes/eventRoute");
 const tasalbarRoute = require("./routes/tasalbarRoute");
-const redisClient = require("./routes/redisClient");
+// const redisClient = require("./routes/redisClient");
 
 const { db } = require("zevbackv2");
 
@@ -106,7 +106,7 @@ app.use(passRoute);
 app.use(parkingRoute);
 app.use(eventRoute);
 app.use(tasalbarRoute);
-app.use(redisClient);
+// app.use(redisClient);
 zuragPack(app);
 
 app.use(aldaaBarigch);
