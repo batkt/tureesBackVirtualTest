95231adb576b9bdc5c3337be1e61ca544baf54a4
batkt <sw10d057@gmail.com>
2025-06-18 22:31:39 +0800

    z

--- routes/parkingRoute.js
+++ routes/parkingRoute.js
@@ -1086,6 +1086,8 @@ async function getDotorZogsoolById(kholbolt, baiguullagiinId, barilgiinId, id) {
 
 async function getAggregateUilchluulegch(kholbolt, baiguullagiinId, barilgiinId, query) {
   const cacheKey = `parkingUilchluulegch:${baiguullagiinId}:${barilgiinId}`;
+  await client.del(cacheKey);
+  console.log("cached -------------->" + JSON.stringify(cacheKey));
   const cached = await client.lRange(cacheKey, 0, -1);
   if (cached) {
     console.log("cached -------------->" + JSON.stringify(cached));
