84f4baddb5af762b4d1b419844eb92fb1246e768
batkt <sw10d057@gmail.com>
2025-06-20 08:01:15 +0800

    console log clear

--- controller/cgw.js
+++ controller/cgw.js
@@ -364,9 +364,6 @@ async function dansniiKhuulgaAvya(token, next, body) {
         body.record = 1;
       if(body.record)
         url = url + (resultValue ? "/?record=" : "/record?record=") + body.record;
-      console.log("--------typeof---------->>" + typeof responseShunuEsekh?.body);
-      console.log("--------typeof---------->>" + typeof resultValue);
-      console.log("--------url---------->>" + url);
     }
     else
     {
