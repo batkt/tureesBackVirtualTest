6a1df148f24028356d8afd91bf002d45a038da97
batkt <sw10d057@gmail.com>
2025-06-21 14:33:23 +0800

    log clear

--- controller/cgw.js
+++ controller/cgw.js
@@ -362,7 +362,6 @@ async function dansniiKhuulgaAvya(token, next, body) {
       body.record = 1;
     if(body.record)
       url = url + (resultValue ? "/?record=" : "/record?record=") + body.record;
-    console.log("-----------url ----------"+ JSON.stringify(url));
     const response = await instance.get(url, { context });
     if (!response.body) {
       if (next) next(new aldaa("Татах хуулга байхгүй"));
