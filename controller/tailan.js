5d060e50ad8ae5ddd927dbf38e38476492fc8ac7
batkt <sw10d057@gmail.com>
2025-06-25 10:29:01 +0800

    z

--- controller/tailan.js
+++ controller/tailan.js
@@ -2099,8 +2099,10 @@ exports.negtgelTailanAvya = asyncHandler(async (req, res, next) => {
               var filtered = khungulultuusKhassanJagsaalt?.filter((a) => moment(a.ognoo).format("YYYY-MM") === moment(b.ognoo).format("YYYY-MM") && a.tailbar === tailbar && a.index === index);
               if(filtered?.length > 0)
               {
-                // filtered?.[0].tulukhDun -= b.khyamdral;
-                // niitTulukhDun -= b.khyamdral;
+                filtered?.forEach((e) => {
+                  e.tulukhDun -= b.khyamdral;
+                })
+                niitTulukhDun -= b.khyamdral;
               }
               else
               {
