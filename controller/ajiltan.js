3a5d2446948916555edb32324113d4120680343d
batkt <sw10d057@gmail.com>
2025-06-27 14:55:32 +0800

    z

--- controller/ajiltan.js
+++ controller/ajiltan.js
@@ -277,14 +277,16 @@ exports.tokenoorAjiltanAvya = asyncHandler(async (req, res, next) => {
   }
 });
 
-exports.nuutsUgShalgakhAjiltan = asyncHandler(tokenShalgakh, async (req, res, next) => {
+exports.nuutsUgShalgakhAjiltan = asyncHandler(async (req, res, next) => {
   try {
     const { db } = require("zevbackv2");
     var ajiltan = await Ajiltan(db.erunkhiiKholbolt).findById(req.body.id);
     var ok = await ajiltan.passwordShalgaya(req.body.nuutsUg);
+    console.log("ok --------- nuutsUgShalgakhAjiltan --------"+ JSON.stringify(ok));
     if (!ok) next(new aldaa("Хэрэглэгчийн нэр эсвэл нууц үг буруу байна!"));
     res.status(200).json(ok);
   } catch (error) {
+    console.log("error --------- nuutsUgShalgakhAjiltan --------"+ JSON.stringify(error));
     next(error);
   }
 });
