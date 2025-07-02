8d09460344129ec772b3d087658b5e867a0b51a9
batkt <sw10d057@gmail.com>
2025-07-02 20:21:31 +0800

    я

--- routes/gereeRoute.js
+++ routes/gereeRoute.js
@@ -1251,7 +1251,7 @@ router
                           $nin: ["baritsaa"],
                         },
                         "avlaga.guilgeenuud.ognoo": {
-                          $lt: new Date(req.body.ekhlekhOgnoo),
+                          $lt: req.body.ekhlekhOgnoo,
                         },
                       },
                     },
