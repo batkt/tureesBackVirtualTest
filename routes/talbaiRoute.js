6faf940e68438c49ccf4ac97d314dca76906f4b3
batkt <sw10d057@gmail.com>
2025-07-25 09:48:47 +0800

    талбайн дугаар давхардаж бна шалуур оруулсан талбай засах дээр

--- routes/talbaiRoute.js
+++ routes/talbaiRoute.js
@@ -208,6 +208,12 @@ function tooZasyaSync(too) {
 router.route("/talbaiZasya").post(tokenShalgakh, async (req, res, next) => {
   try
   {
+    var talbaiShalgakh = await Talbai(req.body.tukhainBaaziinKholbolt).findOne({
+      kod: req.body.kod,
+      baiguullagiinId: req.body.baiguullagiinId,
+      barilgiinId: req.body.barilgiinId,
+    });
+    if (talbaiShalgakh) throw new Error("Талбайн дугаар давхардаж байна!");
     var talbai = new Talbai(req.body.tukhainBaaziinKholbolt)(req.body);
     var khuuchinTalbai = await Talbai(req.body.tukhainBaaziinKholbolt).findById(
       req.body._id
