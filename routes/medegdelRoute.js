cd1bb0416ef3c5c7be806f75d34a94dc16fbcbaa
batkt <sw10d057@gmail.com>
2025-07-29 16:42:17 +0800

    z

--- routes/medegdelRoute.js
+++ routes/medegdelRoute.js
@@ -82,15 +82,17 @@ router
       console.log("log bearerToken ------>>" + JSON.stringify(bearerToken));
       var kholboltuud = db.kholboltuud;
       var kholbolt = kholboltuud.find((a) => a.baiguullagiinId == baiguullaga?.baiguullagiinId);
-      const medegdel = new Sonorduulga(kholbolt)();
-      medegdel.baiguullagiinId = baiguullaga?.baiguullagiinId;
-      medegdel.turul = req.body.turul || "medegdel";
-      medegdel.title = medeelel.title;
-      medegdel.message = medeelel.body;
-      medegdel.kharsanEsekh = false;
-      console.log("log medegdel ------>>" + JSON.stringify(medegdel));
-      await medegdel.save();
-
+      for await (const barilga of baiguullaga.barilguud) {
+        const medegdel = new Sonorduulga(kholbolt)();
+        medegdel.baiguullagiinId = baiguullaga?.baiguullagiinId;
+        medeelel.barilgiinId = barilga._id.toString();
+        medegdel.turul = req.body.turul || "medegdel";
+        medegdel.title = medeelel.title;
+        medegdel.message = medeelel.body;
+        medegdel.kharsanEsekh = false;
+        console.log("log medegdel ------>>" + JSON.stringify(medegdel));
+        await medegdel.save();
+      }
       const io = req.app.get("socketio");
       if (io) io.emit("adminMedegdelilgeeyeSocket" + baiguullaga?.baiguullagiinId, medegdel);
       res.send("done");
