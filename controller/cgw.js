83495d4420ca236bead71891f90ced38efc1e2ff
batkt <sw10d057@gmail.com>
2025-07-31 15:15:39 +0800

    record remove

--- controller/cgw.js
+++ controller/cgw.js
@@ -359,8 +359,6 @@ async function dansniiKhuulgaAvya(token, next, body) {
     const responseShunuEsekh = await instance.get("https://api.khanbank.com/v1/statements/corporate/state", { context }); 
     const resultValue = JSON.parse(responseShunuEsekh?.body);
     url = "https://api.khanbank.com/v1/statements/" + (resultValue ? "corporate/" : "") + body.dansniiDugaar;
-    if(resultValue)
-      body.record = 1;
     if(body.record)
       url = url + (resultValue ? "/?record=" : "/record?record=") + body.record;
     const response = await instance.get(url, { context });
