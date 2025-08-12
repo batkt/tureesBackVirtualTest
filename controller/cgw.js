2b1b94ada53dc7e98618c4398659453a8f1dfef7
batkt <sw10d057@gmail.com>
2025-08-13 00:51:34 +0800

    z

--- controller/cgw.js
+++ controller/cgw.js
@@ -1547,23 +1547,26 @@ exports.bankniiKhuulgaTatyaOirkhon = asyncHandler(async () => {
                     x.baiguullagiinId = dans.baiguullagiinId;
                     x.barilgiinId = dans.barilgiinId;
                   });
-                  // if (guilgeenuud) {
-                  //   var ustgakhJagsaalt = [];
-                  //   for await (const item of guilgeenuud) {
-                  //     var guilgee = await BankniiGuilgee(kholbolt).findOne({
-                  //       record: item.record,
-                  //       code: item.code,
-                  //       branch: item.branch,
-                  //       barilgiinId: dans.barilgiinId,
-                  //     });
-                  //     if (guilgee) ustgakhJagsaalt.push(item);
-                  //   }
-                  //   if (!!ustgakhJagsaalt) {
-                  //     guilgeenuud = guilgeenuud.filter(
-                  //       (el) => !ustgakhJagsaalt.includes(el)
-                  //     );
-                  //   }
-                  // }
+                  if (guilgeenuud) {
+                    var ustgakhJagsaalt = [];
+                    for await (const item of guilgeenuud) {
+                      var indexTalbar =
+                        dans.barilgiinId +
+                        dans.bank +
+                        dans.dansniiDugaar +
+                        item.record +
+                        item.amount.toString();
+                      var guilgee = await BankniiGuilgee(kholbolt).findOne({
+                        indexTalbar: indexTalbar,
+                      });
+                      if (guilgee) ustgakhJagsaalt.push(item);
+                    }
+                    if (!!ustgakhJagsaalt) {
+                      guilgeenuud = guilgeenuud.filter(
+                        (el) => !ustgakhJagsaalt.includes(el)
+                      );
+                    }
+                  }
                   for await (const item of guilgeenuud) {
                     if (!!dans.zogsooliinId) {
                       var url =
