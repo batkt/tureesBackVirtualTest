62feaa08ec4b5eedcab0c5d0a604e9a5f9d411f4
batkt <sw10d057@gmail.com>
2025-09-25 17:13:38 +0800

    z baritsaa

--- controller/tulbur.js
+++ controller/tulbur.js
@@ -1918,6 +1918,9 @@ exports.tsutsalsanGereenuudedZalruulgaOruulya = asyncHandler(async (req, res, ne
       baiguullagiinId: baiguullagiinId,
       barilgiinId: barilgiinId,
       tuluv: -1,
+      "avlaga.guilgeenuud.turul": {
+        $nin: ["baritsaa"],
+      },
     };
     if(req.body?.gereeniiDugaar)
       match["gereeniiDugaar"] = req.body?.gereeniiDugaar;
