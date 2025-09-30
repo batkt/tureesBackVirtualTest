559d6ad3e04cfdfe8d893b77ed5bc96712fbeb20
batkt <sw10d057@gmail.com>
2025-09-30 19:34:47 +0800

    aldangi ad

--- controller/tailan.js
+++ controller/tailan.js
@@ -1688,7 +1688,7 @@ exports.negtgelMedeelelAvya = asyncHandler(async (req, res, next) => {
         },
         {
           $group: {
-            _id: { turul: { $ifNull: ["$avlaga.guilgeenuud.aldangiinTurul", "tulsunAldangi"] }, register: "$register" },
+            _id: { turul: { $ifNull: [{ $concat: ["$avlaga.guilgeenuud.aldangiinTurul", "aldangi"] }, "tulsunAldangi"] }, register: "$register" },
             dun: {
               $sum: "$avlaga.guilgeenuud.tulsunAldangi",
             },
