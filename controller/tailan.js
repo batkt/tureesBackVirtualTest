25a6bb18cba81592d2712d847baafe721e9cfc70
batkt <sw10d057@gmail.com>
2025-10-16 12:06:46 +0800

    0 zero

--- controller/tailan.js
+++ controller/tailan.js
@@ -2061,7 +2061,7 @@ exports.negtgelMedeelelAvya = asyncHandler(async (req, res, next) => {
         if (req.body.ajiltnaarAvakhEsekh) {
           var data = [];
           for (const ajiltan of ajiltnuud) {
-            if (!!ajiltan?._id?.burtgesenAjiltaniiNer) {
+            if (!!ajiltan?._id?.burtgesenAjiltaniiNer && ajiltan?._id?.burtgesenAjiltaniiId !== 0) {
               var ajiltanData = await Ajiltan(db.erunkhiiKholbolt).findById(ajiltan?._id?.burtgesenAjiltaniiId);
               var mur = {
                 ajiltanRegister: ajiltanData?.register ? ajiltanData?.register : ajiltan?._id?.burtgesenAjiltaniiNer,
