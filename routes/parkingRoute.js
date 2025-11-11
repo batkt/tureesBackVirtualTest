79bcdbc6ce688cec63da541a111e7730d415d02d
Hanguri <hanguri78@gmail.com>
2025-11-11 12:19:22 +0800

    niit bodogdson zasav

--- routes/parkingRoute.js
+++ routes/parkingRoute.js
@@ -1050,6 +1050,8 @@ router.post(
         //"tuukh.zogsooliinId": req.body.zogsooliinId,
       };
       if (!!req.body.barilgiinId) match.barilgiinId = req.body.barilgiinId;
+      const ekhlekhOgnoo = new Date(req.body.ekhlekhOgnoo);
+      const duusakhOgnoo = new Date(req.body.duusakhOgnoo);
       const query = [
         {
           $match: match,
@@ -1061,8 +1063,8 @@ router.post(
         {
           $match: {
             "tuukh.tulbur.ognoo": {
-              $gte: new Date(req.body.ekhlekhOgnoo),
-              $lte: new Date(req.body.duusakhOgnoo),
+              $gte: ekhlekhOgnoo,
+              $lte: duusakhOgnoo,
             },
           },
         },
@@ -1119,10 +1121,54 @@ router.post(
           },
         },
       ];
-      const khariu = await Uilchluulegch(
+      let khariu = await Uilchluulegch(
         req.body.tukhainBaaziinKholbolt,
         true
       ).aggregate(query);
+      const zurchilUneguinMatch = {
+        "tuukh.tsagiinTuukh.garsanTsag": {
+          $gte: ekhlekhOgnoo,
+          $lte: duusakhOgnoo,
+        },
+        $or: [
+          { "tuukh.tuluv": -2 },
+          { "tuukh.uneguiGarsan": { $exists: true } },
+        ],
+      };
+      if (req.body.garakhKhaalgaIp) {
+        zurchilUneguinMatch["tuukh.garsanKhaalga"] = req.body.garakhKhaalgaIp;
+      }
+      const zurchilUneguinNiit = await Uilchluulegch(
+        req.body.tukhainBaaziinKholbolt,
+        true
+      ).aggregate([
+        { $match: match },
+        { $unwind: "$tuukh" },
+        { $match: zurchilUneguinMatch },
+        {
+          $group: {
+            _id: null,
+            niitDun: { $sum: { $ifNull: ["$tuukh.tulukhDun", 0] } },
+          },
+        },
+      ]);
+      const zurchilUneguinDun =
+        zurchilUneguinNiit && zurchilUneguinNiit.length > 0
+          ? zurchilUneguinNiit[0].niitDun || 0
+          : 0;
+      if (!khariu || khariu.length === 0) {
+        khariu = [
+          {
+            _id: "id",
+            dun: 0,
+            garsanKhaalga: 0,
+            niitDun: 0,
+            khungulsun: 0,
+          },
+        ];
+      }
+      khariu[0].niitDun =
+        (khariu[0].niitDun ? khariu[0].niitDun : 0) + zurchilUneguinDun;
       res.send(khariu);
     } catch (err) {
       next(err);
