2036cf36cfe87056fc9188f09142e957c475b479
batkt <sw10d057@gmail.com>
2025-11-13 10:46:10 +0800

    aldangiinKhuvi: Number, geree

--- models/geree.js
+++ models/geree.js
@@ -148,6 +148,7 @@ const gereeSchema = new Schema(
     garaasKhonogOruulakhEsekh: Boolean,
     ekhniiSariinKhonog: Number,
     nekhemjlekhiinOgnoo: Date,
+    aldangiinKhuvi: Number,
     zurguud: [String],
     zardluud: [
       {
