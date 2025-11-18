3113cadab782d7e2d90ae3e1b929787c056af6be
batkt <sw10d057@gmail.com>
2025-11-18 11:47:11 +0800

    z z ГД2511181

--- controller/tulbur.js
+++ controller/tulbur.js
@@ -787,7 +787,7 @@ module.exports.aldangiBodyo = async function aldangiBodyo(
                 baiguullagiinId: baiguullaga._id.toString(),
                 barilgiinId: barilga._id.toString(),
                 tuluv: { $nin: [-1] },
-                gereeniiDugaar: "ГД2510063",
+                gereeniiDugaar: "ГД2511181",
               },
             },
             { $unwind: "$avlaga.guilgeenuud" },
