0f0b39af965ab0339165a78cd9a561554b2c5e67
batkt <sw10d057@gmail.com>
2025-11-24 19:20:43 +0800

    z

--- controller/tulbur.js
+++ controller/tulbur.js
@@ -965,7 +965,7 @@ module.exports.aldangiBodyo = async function aldangiBodyo(
                   ognoo: aldangiEhlehOgnoo,
                   uldegdel: uldegdel,
                   aldangiChuluulukhOgnoo: aldangiChuluulukhOgnoo,
-                  aldangiBodsonOgnoo: moment().subtract(3, "days"), // new Date(),
+                  aldangiBodsonOgnoo: moment().subtract(2, "days"), // new Date(),
                   aldangiinKhuvi: aldagiinKhuvi,
                   aldangiChuluulukhKhonog: aldangiChuluulukhKhonog,
                   aldangi: bodogdsonKhuu,
