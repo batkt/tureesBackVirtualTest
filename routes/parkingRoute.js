97ef2b7b40162dd0e04d9019c2ba907ce64d5484
buynaaaa24 <abuynaa1211@gmail.com>
2025-12-08 15:44:18 +0800

    niitdun butsaalaa

--- routes/parkingRoute.js
+++ routes/parkingRoute.js
@@ -1179,7 +1179,7 @@ router.post(
           $group: {
             _id: null,
             niitDun: { $sum: "$niitDun" },
-            niitEbarimt: { $sum: { $ifNull: ["$ebarimtAvsanDun", 0] } },
+            niitEbarimt: { $sum: "$ebarimtAvsanDun" },
             niitToo: { $sum: 1 },
           },
         },
@@ -1193,13 +1193,12 @@ router.post(
       const tulburPipeline = [
         { $match: query },
         { $unwind: "$tuukh" },
+        { $unwind: "$tuukh.tulbur" },
         {
           $match: {
             "tuukh.tuluv": { $in: [1, 2] },
-            "tuukh.tulbur": { $exists: true, $ne: [] },
           },
         },
-        { $unwind: "$tuukh.tulbur" },
         {
           $group: {
             _id: null,
@@ -1220,16 +1219,8 @@ router.post(
         totalToo: result[0]?.niitToo || 0,
       };
 
-      console.log("Summary calculation:", {
-        query: JSON.stringify(query),
-        result: result[0],
-        tulburResult: tulburResult[0],
-        summary,
-      });
-
       res.send(summary);
     } catch (err) {
-      console.error("Summary error:", err);
       next(err);
     }
   }
