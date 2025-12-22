8add957532053bfb279aa50fee090daf511d7189
batkt <sw10d057@gmail.com>
2025-12-22 16:10:19 +0800

    z

--- controller/cgw.js
+++ controller/cgw.js
@@ -848,11 +848,11 @@ exports.bankniiKhuulgaTatajKhadgalya = asyncHandler(async (req, res, next) => {
                   corporateShunuUntraakhEsekh: dans.corporateShunuUntraakhEsekh,
                 };
                 if(dans.barilgiinId == "6735c77a7fc60cd66deb290a" && dans.dugaar == "5100229713")
-                  bodyKhuulga["record"] = 31106;
+                  bodyKhuulga["record"] = 31107;
                 else
-                {
-                  if (max && max.length !== 0) bodyKhuulga["record"] = max[0].max;
-                }
+                  {
+                    if (max && max.length !== 0) bodyKhuulga["record"] = max[0].max;
+                  }
                 var khariu = await dansniiKhuulgaAvya(token, next, bodyKhuulga);
 
                 if (khariu && khariu.transactions) {
