2d1d34e8bc903dae74ff50ff79ac6aec31893380
batkt <sw10d057@gmail.com>
2025-12-31 19:34:40 +0800

    console log

--- controller/cgw.js
+++ controller/cgw.js
@@ -1524,8 +1524,6 @@ exports.bankniiKhuulgaTatyaOirkhon = asyncHandler(async () => {
     var dansnuud;
     if (kholboltuud) {
       for await (const kholbolt of kholboltuud) {
-        if(kholbolt.baiguullagiinId !== "65cf2f027fbc788f85e50b90") continue;
-        console.log("Oirkhon tatakh guilgee: " + kholbolt.baiguullagiinId);
         var firstDay;
         var lastDay;
         firstDay = new Date(new Date().getFullYear(), new Date().getMonth(), 1);
