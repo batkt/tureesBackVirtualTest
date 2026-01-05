8ac2f0c62c742fec4f8066b00c472c30b61b7434
batkt <sw10d057@gmail.com>
2026-01-05 11:41:36 +0800

    я

--- controller/cgw.js
+++ controller/cgw.js
@@ -363,10 +363,10 @@ async function dansniiKhuulgaAvya(token, next, body) {
       "https://api.khanbank.com/v1/statements/" +
       (resultValue ? "corporate/" : "") +
       body.dansniiDugaar;
-    if(body.ekhlekhOgnoo && body.duusakhOgnoo)
-    {
-      url = url + "?from=" + body.ekhlekhOgnoo + "&to=" + body.duusakhOgnoo + "&page=" + body.khuudasniiDugaar + "&&size=" + body.khuudasniiKhemjee;
-    }
+    // if(body.ekhlekhOgnoo && body.duusakhOgnoo)
+    // {
+    //   url = url + "?from=" + body.ekhlekhOgnoo + "&to=" + body.duusakhOgnoo + "&page=" + body.khuudasniiDugaar + "&&size=" + body.khuudasniiKhemjee;
+    // }
     if (body.record)
       url = url + (resultValue ? "" : "/record?record=" + body.record);
     const response = await instance.get(url, { context });
@@ -851,17 +851,18 @@ exports.bankniiKhuulgaTatajKhadgalya = asyncHandler(async (req, res, next) => {
                   dansniiDugaar: dans.dugaar,
                   corporateShunuUntraakhEsekh: dans.corporateShunuUntraakhEsekh,
                 };
-                if(dans.barilgiinId == "6735c77a7fc60cd66deb290a" && dans.dugaar == "5100229713")
-                {
-                  bodyKhuulga.ekhlekhOgnoo = "20251021"
-                  bodyKhuulga.duusakhOgnoo = "20251031"
-                  bodyKhuulga.khuudasniiDugaar = 0;
-                  bodyKhuulga.khuudasniiKhemjee = 100;
-                }
-                else
-                {
-                  if (max && max.length !== 0) bodyKhuulga["record"] = max[0].max;
-                }
+                // if(dans.barilgiinId == "6735c77a7fc60cd66deb290a" && dans.dugaar == "5100229713")
+                // {
+                //   bodyKhuulga.ekhlekhOgnoo = "20251021"
+                //   bodyKhuulga.duusakhOgnoo = "20251031"
+                //   bodyKhuulga.khuudasniiDugaar = 0;
+                //   bodyKhuulga.khuudasniiKhemjee = 100;
+                // }
+                // else
+                // {
+                //   if (max && max.length !== 0) bodyKhuulga["record"] = max[0].max;
+                // }
+                if (max && max.length !== 0) bodyKhuulga["record"] = max[0].max;
                 var khariu = await dansniiKhuulgaAvya(token, next, bodyKhuulga);
 
                 if (khariu && khariu.transactions) {
