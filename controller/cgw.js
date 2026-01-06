8f251368fcff665880b21b03e034b2dd17e79b3c
batkt <sw10d057@gmail.com>
2026-01-06 13:51:58 +0800

    z 9

--- controller/cgw.js
+++ controller/cgw.js
@@ -854,8 +854,8 @@ exports.bankniiKhuulgaTatajKhadgalya = asyncHandler(async (req, res, next) => {
                 };
                 if(dans.barilgiinId == "6735c77a7fc60cd66deb290a" && dans.dugaar == "5100229713")
                 {
-                  bodyKhuulga.ekhlekhOgnoo = "20251201"
-                  bodyKhuulga.duusakhOgnoo = "20251216"
+                  bodyKhuulga.ekhlekhOgnoo = "20251217"
+                  bodyKhuulga.duusakhOgnoo = "20251231"
                   bodyKhuulga.khuudasniiDugaar = 0;
                   bodyKhuulga.khuudasniiKhemjee = 100;
                 }
