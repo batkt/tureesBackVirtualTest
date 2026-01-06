64db46015510dbc1dcc271720deb7c5dec7f22f8
batkt <sw10d057@gmail.com>
2026-01-06 14:02:52 +0800

    z 9

--- controller/cgw.js
+++ controller/cgw.js
@@ -854,8 +854,8 @@ exports.bankniiKhuulgaTatajKhadgalya = asyncHandler(async (req, res, next) => {
                 };
                 if(dans.barilgiinId == "6735c77a7fc60cd66deb290a" && dans.dugaar == "5100229713")
                 {
-                  bodyKhuulga.ekhlekhOgnoo = "20251117"
-                  bodyKhuulga.duusakhOgnoo = "20251130"
+                  bodyKhuulga.ekhlekhOgnoo = "20251101"
+                  bodyKhuulga.duusakhOgnoo = "20251116"
                   bodyKhuulga.khuudasniiDugaar = 0;
                   bodyKhuulga.khuudasniiKhemjee = 100;
                 }
