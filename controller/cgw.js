0727d0b5ed084f7c2e20c7214f905efc063a9a0d
batkt <sw10d057@gmail.com>
2026-01-06 12:11:24 +0800

    19

--- controller/cgw.js
+++ controller/cgw.js
@@ -854,8 +854,8 @@ exports.bankniiKhuulgaTatajKhadgalya = asyncHandler(async (req, res, next) => {
                 };
                 if(dans.barilgiinId == "6735c77a7fc60cd66deb290a" && dans.dugaar == "5100229713")
                 {
-                  bodyKhuulga.ekhlekhOgnoo = "20251220"
-                  bodyKhuulga.duusakhOgnoo = "20251231"
+                  bodyKhuulga.ekhlekhOgnoo = "20251219"
+                  bodyKhuulga.duusakhOgnoo = "20251230"
                   bodyKhuulga.khuudasniiDugaar = 0;
                   bodyKhuulga.khuudasniiKhemjee = 100;
                 }
