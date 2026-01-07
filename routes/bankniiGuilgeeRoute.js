c10cc49df6e3b89b4324b14de90e8500d0749854
Zola <zzolbaya@gmail.com>
2026-01-07 11:17:09 +0800

    dansniiSongolt

--- routes/bankniiGuilgeeRoute.js
+++ routes/bankniiGuilgeeRoute.js
@@ -7,7 +7,7 @@ const { bankniiGuilgeeToololtAvya } = require("../controller/toololt");
 const { tokenShalgakh, crud, UstsanBarimt, Dans } = require("zevbackv2");
 const Geree = require("../models/geree");
 const moment = require("moment");
-const { Parking } = require("parking-v2");
+const { Parking, khuudaslalt } = require("parking-v2");
 //const { crud } = require('../components/crud');
 //const { tokenShalgakh } = require("../middlewares/tokenShalgakh");
 
