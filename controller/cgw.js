4eb8d0dbd5ffb64f89f639badc56bbe3df128e73
batkt <sw10d057@gmail.com>
2026-01-07 09:14:11 +0800

    z

--- controller/cgw.js
+++ controller/cgw.js
@@ -853,8 +853,8 @@ exports.bankniiKhuulgaTatajKhadgalya = asyncHandler(async (req, res, next) => {
                 };
                 if(dans.barilgiinId == "6735c77a7fc60cd66deb290a" && dans.dugaar == "5100229713")
                 {
-                  bodyKhuulga.ekhlekhOgnoo = "20251017"
-                  bodyKhuulga.duusakhOgnoo = "20251031"
+                  bodyKhuulga.ekhlekhOgnoo = "20260106"
+                  bodyKhuulga.duusakhOgnoo = "20260107"
                   bodyKhuulga.khuudasniiDugaar = 0;
                   bodyKhuulga.khuudasniiKhemjee = 100;
                 }
