e5eb0d82d2b0838aac2cb291c76473402727e77a
buynaaaa24 <abuynaa1211@gmail.com>
2026-01-08 08:59:10 +0800

    a

--- index.js
+++ index.js
@@ -66,14 +66,14 @@ app.use(
     extended: true,
   })
 );
-//test
-// db.kholboltUusgey(
-//   app,
-//   "mongodb://admin:Br1stelback1@127.0.0.1:27017/turees?authSource=admin"
-// );
+test;
+db.kholboltUusgey(
+  app,
+  "mongodb://admin:Br1stelback1@127.0.0.1:27017/turees?authSource=admin"
+);
 
 //production
-db.kholboltUusgey(app);
+// db.kholboltUusgey(app);
 
 app.use(
   express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 })
