7a3341fedbc5fa59ca0f5faf2cf533a6c092adb2
batkt <sw10d057@gmail.com>
2026-01-12 17:15:52 +0800

    bankniii

--- index.js
+++ index.js
@@ -111,16 +111,16 @@ zuragPack(app);
 
 app.use(aldaaBarigch);
 
-// cron.schedule(
-//   "*/5 * * * * ",
-//   function () {
-//     cgw.bankniiKhuulgaTatajKhadgalya(null, null, null);
-//   },
-//   {
-//     scheduled: true,
-//     timezone: "Asia/Ulaanbaatar",
-//   }
-// );
+cron.schedule(
+  "*/5 * * * * ",
+  function () {
+    cgw.bankniiKhuulgaTatajKhadgalya(null, null, null);
+  },
+  {
+    scheduled: true,
+    timezone: "Asia/Ulaanbaatar",
+  }
+);
 
 cron.schedule(
   "*/4 * * * * * ",
