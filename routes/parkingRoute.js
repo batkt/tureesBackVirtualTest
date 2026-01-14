78ced17b639ad33e9d3e470d052d805ee4b75f8b
batkt <sw10d057@gmail.com>
2026-01-14 13:19:51 +0800

    z

--- routes/parkingRoute.js
+++ routes/parkingRoute.js
@@ -539,43 +539,8 @@ router.post("/zogsoolUstgay", tokenShalgakh, async (req, res, next) => {
 });
 
 router.post("/zogsoolSdkService", tokenShalgakh, async (req, res, next) => {
-  // Safe logging function to handle circular references
-  const safeStringify = (obj) => {
-    const seen = new WeakSet();
-    return JSON.stringify(
-      obj,
-      (key, value) => {
-        // Skip circular references
-        if (typeof value === "object" && value !== null) {
-          if (seen.has(value)) {
-            return "[Circular]";
-          }
-          seen.add(value);
-        }
-        // Skip Mongoose models and functions
-        if (value && typeof value === "object") {
-          if (value.constructor && value.constructor.name === "model") {
-            return "[Mongoose Model]";
-          }
-          if (typeof value.toObject === "function") {
-            try {
-              return value.toObject();
-            } catch (e) {
-              return "[Mongoose Object]";
-            }
-          }
-        }
-        if (typeof value === "function") {
-          return "[Function]";
-        }
-        return value;
-      },
-      2
-    );
-  };
-  // console.log("zogsoolSdkService - Request Body:", safeStringify(req.body));
-  try {
-    console.log("SDK Zogsool SDK Service called" + JSON.stringify(req.body));
+  try 
+  {
     if (req.body.mashiniiDugaar)
       req.body.mashiniiDugaar = req.body.mashiniiDugaar.replace(/\0/g, "");
     if (!!req?.body?.color) {
