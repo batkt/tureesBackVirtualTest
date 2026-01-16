9fe150cdebf22204fad80728a4ee2b5376df9f12
Zola <zzolbaya@gmail.com>
2026-01-16 18:54:31 +0800

    zasla

--- routes/bankniiGuilgeeRoute.js
+++ routes/bankniiGuilgeeRoute.js
@@ -282,7 +282,7 @@ router.get(
           console.log("  Query:", JSON.stringify(queryBody.query, null, 2));
 
           try {
-            console.log("------------------>>>>>>>", khuudasniiKhemjee);
+            console.log("------------------>>>>>>>", body?.khuudasniiKhemjee);
             const result = await khuudaslalt(model, queryBody);
             const count = result.jagsaalt?.length || 0;
 
