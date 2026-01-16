924607c88e4e55c6033d1998368d1cc3975ad2d7
batkt <sw10d057@gmail.com>
2026-01-16 16:58:15 +0800

    index

--- index.js
+++ index.js
@@ -307,7 +307,7 @@ cron.schedule(
 );
 
 cron.schedule(
-  "45 16 * * * ",
+  "10 1 * * * ",
   function () {
     cgw.archiveBankGuilgeeKhonog();
   },
@@ -318,7 +318,7 @@ cron.schedule(
 );
 
 cron.schedule(
-  "46 16 * * * ",
+  "20 1 5 * * ",
   function () {
     cgw.archiveBankGuilgeeRently();
   },
