57cfe6b3316cabb74613e61076a3578205a930b7
batkt <sw10d057@gmail.com>
2026-01-16 16:29:40 +0800

    33 35

--- index.js
+++ index.js
@@ -307,7 +307,7 @@ cron.schedule(
 );
 
 cron.schedule(
-  "10 1 * * * ",
+  "33 16 * * * ",
   function () {
     cgw.archiveBankGuilgeeKhonog();
   },
@@ -318,7 +318,7 @@ cron.schedule(
 );
 
 cron.schedule(
-  "20 1 5 * * ",
+  "35 16 * * * ",
   function () {
     cgw.archiveBankGuilgeeRently();
   },
