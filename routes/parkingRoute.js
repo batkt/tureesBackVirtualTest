4dc6494607c9011f59ea1c65d93353f8973e159e
batkt <sw10d057@gmail.com>
2026-02-06 15:44:43 +0800

    z

--- routes/parkingRoute.js
+++ routes/parkingRoute.js
@@ -1824,17 +1824,20 @@ function stableStringify(obj) {
 }
 
 async function getParkingFind(kholbolt, baiguullagiinId, query) {
+  
+  const buf = encode({ hello: "world" });
+  const obj = decode(buf);
   const queryKey = crypto
     .createHash("md5")
     .update(stableStringify(query))
     .digest("hex");
   const cacheKey = `parkingFind:${baiguullagiinId}:${queryKey}`;
   const cached = await client.get(cacheKey);
-  if (cached) return decode(cached);
-  const data = await Parking(kholbolt).find(query).lean();
-  if(data?.length > 0) {
-    await client.setEx(cacheKey, 300, Buffer.from(encode(data)));
-  }
+  if (cached) return JSON.parse(cached);
+  const data = await Parking(kholbolt)
+    .find(query)
+    .lean();
+  await client.setEx(cacheKey, 60, JSON.stringify(data));
   return data;
 }
 
