701a2d0fb2b3736c6508a92961104e3798a9c891
buynaaaa24 <abuynaa1211@gmail.com>
2026-02-28 15:40:56 +0800

    butsaala

--- routes/gereeRoute.js
+++ routes/gereeRoute.js
@@ -2539,40 +2539,48 @@ router
         {
           $facet: {
             baritsaaAshiglasanDun: [
-  {
-    $unwind: {
-      path: "$avlaga.baritsaa",  
-    },
-  },
-  {
-    $match: {
-      "avlaga.baritsaa.ognoo": {
-        $gte: new Date(req.body.ekhlekhOgnoo),
-        $lte: new Date(req.body.duusakhOgnoo),
-      },
-      "avlaga.baritsaa.orlogo": {   
-        $gt: 0,
-      },
-    },
-  },
-  {
-    $group: {
-      _id: "$gereeniiDugaar",
-      tulsun: {
-        $sum: {
-          $ifNull: ["$avlaga.baritsaa.orlogo", 0],
-        },
-      },
-    },
-  },
-  {
-    $project: {
-      _id: 0,
-      gereeniiDugaar: "$_id",
-      uldegdel: "$tulsun",
-    },
-  },
-],
+              {
+                $unwind: {
+                  path: "$avlaga.guilgeenuud",
+                },
+              },
+              {
+                $match: {
+                  "avlaga.guilgeenuud.ognoo": {
+                    $gte: new Date(req.body.ekhlekhOgnoo),
+                    $lt: new Date(req.body.nekhemjlekhAvakhOgnoo),
+                  },
+                  $and: [
+                    {
+                      "avlaga.guilgeenuud.turul": {
+                        $in: ["baritsaa"],
+                      },
+                    },
+                    {
+                      "avlaga.guilgeenuud.tulsunDun": {
+                        $gt: 0,
+                      },
+                    },
+                  ],
+                },
+              },
+              {
+                $group: {
+                  _id: "$gereeniiDugaar",
+                  tulsun: {
+                    $sum: {
+                      $ifNull: ["$avlaga.guilgeenuud.tulsunDun", 0],
+                    },
+                  },
+                },
+              },
+              {
+                $project: {
+                  gereeniiDugaar: "$gereeniiDugaar",
+                  uldegdel: "$tulsun",
+                },
+              },
+            ],
             umnukhSariinTulsun: [
               {
                 $unwind: {
