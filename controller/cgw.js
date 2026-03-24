af64b7b08ffd40614b24f52b42ed45c027515bfc
batkt <sw10d057@gmail.com>
2026-03-24 11:59:58 +0800

    z 20 cwg

--- controller/cgw.js
+++ controller/cgw.js
@@ -979,10 +979,14 @@ exports.bankniiKhuulgaTatajKhadgalya = asyncHandler(async (req, res, next) => {
                     })
                     .sort({ TxDt: -1 })
                     .limit(1);
-                  if (!!max) {
-                    firstDay = new Date(max.TxDt);
-                  } else firstDay = new Date();
-                  // firstDay = new Date(new Date().getFullYear(), new Date().getMonth(), 1);
+                  // if (!!max) {
+                  //   firstDay = new Date(max.TxDt);
+                  // } else firstDay = new Date();
+                  firstDay = new Date(
+                    new Date().getFullYear(),
+                    new Date().getMonth(),
+                    20,
+                  );
                   url =
                     url +
                     "?from=" +
