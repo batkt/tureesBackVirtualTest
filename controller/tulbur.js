ab9e79b7197090d40e165310cae788a728459a58
batkt <sw10d057@gmail.com>
2026-03-25 09:10:20 +0800

    z

--- controller/tulbur.js
+++ controller/tulbur.js
@@ -360,10 +360,12 @@ exports.gereeniiGuilgeeKhadgalya = asyncHandler(async (req, res, next) => {
       inc["niitTulsunAldangi"] = +guilgee.tulsunAldangi;
     }
     if (guilgee.gereeniiId) {
+      talbainIdnuud;
       console.log("tooluuriinDugaar", guilgee.tooluuriinDugaar);
       var tempGeree = Geree(req.body.tukhainBaaziinKholbolt).findById(
         guilgee.gereeniiId,
       );
+      console.log("talbainIdnuud ---->", tempGeree?.talbainIdnuud);
       if (tempGeree?.talbainIdnuud?.length > 0) {
         for (const talbainId of tempGeree.talbainIdnuud) {
           var talbaiData = Talbai(req.body.tukhainBaaziinKholbolt).findById(
