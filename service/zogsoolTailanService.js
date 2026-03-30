421e66cf72da225caceb86e041ae07409d50ec4e
batkt <sw10d057@gmail.com>
2026-03-30 13:41:13 +0800

    tailan

--- service/zogsoolTailanService.js
+++ service/zogsoolTailanService.js
@@ -261,17 +261,14 @@ async function udriinTailan({ body }) {
                 ? "Төлбөртэй"
                 : "Үнэгүй",
           niitDun: { $sum: "$niitDun" },
-          ids: { $addToSet: "$_id" },
+          niitToo: { $sum: 1 },
         },
       },
       {
         $project: {
           _id: 1,
           niitDun: 1,
-          niitToo:
-            status === "Zurchiltei" || status === "Tulburtei"
-              ? { $size: "$ids" }
-              : 1,
+          niitToo: 1,
         },
       },
     ];
