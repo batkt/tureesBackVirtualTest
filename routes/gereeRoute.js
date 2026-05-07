009297300efad93e9e428e01e18c01d6a3c71f3c
buynaaaa24 <abuynaa1211@gmail.com>
2026-05-07 09:15:42 +0800

    geree burtgeh uyiin shalguur

--- routes/gereeRoute.js
+++ routes/gereeRoute.js
@@ -482,30 +482,33 @@ crud(
         unuudur.getMonth(),
         unuudur.getDate(),
       );
-      var maxDugaar = 1;
-      await Dugaarlalt(req.body.tukhainBaaziinKholbolt)
-        .find({
+      var dugaarlalt = null;
+      if (req.body.gereeniiDugaar && req.body.gereeniiDugaar.match(/^ГД\d{6}$/)) {
+        var maxDugaar = 1;
+        await Dugaarlalt(req.body.tukhainBaaziinKholbolt)
+          .find({
+            baiguullagiinId: req.body.baiguullagiinId,
+            barilgiinId: req.body.barilgiinId,
+            turul: "geree",
+            ognoo: unuudur,
+          })
+          .sort({
+            dugaar: -1,
+          })
+          .limit(1)
+          .then((result) => {
+            if (result != 0) maxDugaar = result[0].dugaar + 1;
+          });
+        dugaarlalt = new Dugaarlalt(req.body.tukhainBaaziinKholbolt)({
           baiguullagiinId: req.body.baiguullagiinId,
           barilgiinId: req.body.barilgiinId,
+          dugaar: maxDugaar,
           turul: "geree",
           ognoo: unuudur,
-        })
-        .sort({
-          dugaar: -1,
-        })
-        .limit(1)
-        .then((result) => {
-          if (result != 0) maxDugaar = result[0].dugaar + 1;
+          isNew: true,
         });
-      var dugaarlalt = new Dugaarlalt(req.body.tukhainBaaziinKholbolt)({
-        baiguullagiinId: req.body.baiguullagiinId,
-        barilgiinId: req.body.barilgiinId,
-        dugaar: maxDugaar,
-        turul: "geree",
-        ognoo: unuudur,
-        isNew: true,
-      });
-      req.body.gereeniiDugaar = req.body.gereeniiDugaar + maxDugaar;
+        req.body.gereeniiDugaar = req.body.gereeniiDugaar + maxDugaar;
+      }
 
       if (req.body.gereeniiDugaar) {
         const existingGeree = await Geree(req.body.tukhainBaaziinKholbolt, true).findOne({
@@ -520,7 +523,7 @@ crud(
       khariltsagch
         .save()
         .then((result) => {
-          dugaarlalt.save();
+          if (dugaarlalt) dugaarlalt.save();
           next();
         })
         .catch((err) => {
@@ -543,7 +546,7 @@ router.route("/gereeKhadgalya").post(tokenShalgakh, async (req, res, next) => {
   khariltsagch.id = khariltsagch.register
     ? khariltsagch.register
     : khariltsagch.customerTin;
-  if (req.body.gereeniiDugaar === `ГД${moment(new Date()).format("YYMMDD")}`) {
+  if (req.body.gereeniiDugaar && req.body.gereeniiDugaar.match(/^ГД\d{6}$/)) {
     var unuudur = new Date();
     unuudur = new Date(
       unuudur.getFullYear(),
