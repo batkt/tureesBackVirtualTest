d34ce95eb3c1b13287515e83e7402a599d903afc
batkt <sw10d057@gmail.com>
2026-05-20 07:03:42 +0800

    z register

--- controller/tulbur.js
+++ controller/tulbur.js
@@ -1010,6 +1010,7 @@ module.exports.tulultTaniyaCGW = async function tulultTaniyaCGW(
                 for (const t of tailbar) {
                   for (const field of searchFields) {
                     console.log(`Searching for ${t} in field ${field}`);
+                    if (field === "register") t = t?.toUpperCase();
                     geree = await Geree(kholbolt, true).findOne({
                       [field]: t,
                       tuluv: 1,
