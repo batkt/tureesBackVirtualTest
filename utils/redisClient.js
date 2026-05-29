b91cade399965a7f46454a65cd1a679c25204ac0
batkt <sw10d057@gmail.com>
2026-05-29 14:43:48 +0800

    console log add

--- utils/redisClient.js
+++ utils/redisClient.js
@@ -5,6 +5,9 @@ const redisUrl = "redis://127.0.0.1:6379";
 const pubClient = createClient({ url: redisUrl });
 const subClient = pubClient.duplicate();
 
+pubClient.on("error", (err) => console.error("Redis pubClient error:", err));
+subClient.on("error", (err) => console.error("Redis subClient error:", err));
+
 async function connectRedis() {
   if (!pubClient.isOpen) {
     await pubClient.connect();
